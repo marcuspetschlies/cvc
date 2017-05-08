433d8d052856a6a1c44adab12b6bcff512ccb29a
Marcus Petschlies <marcus.petschlies@web.de>
2017-05-08 18:08:46 +0200

    added prototypes for pure and mixed fully factored contractions

--- contract_cvc_tensor.h
+++ contract_cvc_tensor.h
@@ -36,6 +36,10 @@ int contract_vdag_gloc_w_blocked (double**V, int numV, int momentum_number, int 
 
 int contract_vdag_gloc_phi_blocked (double**V, double**Phi, int numV, int numPhi, int momentum_number, int (*momentum_list)[3], int gamma_id_number, int*gamma_id_list, struct AffWriter_s*affw, char*tag, int io_proc, double*gauge_field, double **mzz[2], double**mzzinv[2], int block_size );
 
+int contract_vdag_cvc_w_blocked (double**V, int numV, int momentum_number, int (*momentum_list)[3], struct AffWriter_s*affw, char*tag, int io_proc, double*gauge_field, double **mzz[2], double**mzzinv[2], int block_size );
+
+int contract_vdag_cvc_phi_blocked (double**V, double**Phi, int numV, int numPhi, int momentum_number, int (*momentum_list)[3], struct AffWriter_s*affw, char*tag, int io_proc, double*gauge_field, double **mzz[2], double**mzzinv[2], int block_size );
+
 
 }  /* end of namespace cvc */
 
