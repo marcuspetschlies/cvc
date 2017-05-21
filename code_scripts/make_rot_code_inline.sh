1ceb8f20d28b2095c713aabc0c846fc682c44e44
Marcus Petschlies <marcus.petschlies@web.de>
2017-05-21 15:10:36 +0200

    added code generation for spinor propagator type

--- code_scripts/make_rot_code_inline.sh
+++ code_scripts/make_rot_code_inline.sh
@@ -31,7 +31,7 @@ echo "}  /* end of rot_bispinor_mat_ti_fv */"
 
 fi
 
-# if [ 0 -eq  1 ]; then
+if [ 0 -eq  1 ]; then
 
 echo "inline void rot_fp_ti_bispinor_mat ( fermion_propagator_type _r, double _Complex ** _R, fermion_propagator_type _s) {"
 echo "  double _Complex _c;"
@@ -65,10 +65,10 @@ echo "  _fp_eq_fp( _r, _fp);"
 echo "  free_fp( &_fp);"
 echo "}  /* end of rot_fp_bispinor_mat */"
 
-# fi
+fi
 
 
-#if [ 0 -eq  1 ]; then
+if [ 0 -eq  1 ]; then
 
 echo "inline void rot_fv_ti_bispinor_mat ( double** _r, double _Complex ** _R, double** _s, unsigned int _ix) {"
 echo "  fermion_propagator_type _fp;"
@@ -79,4 +79,53 @@ echo "  _assign_fp_field_from_point( _r, _fp, _ix);"
 echo "  free_fp( &_fp );"
 echo "}  /* end of rot_fv_ti_bispinor_mat */"
 
-#fi
+fi
+
+# if [ 0 -eq  1 ]; then
+
+echo "inline void rot_sp_ti_bispinor_mat ( spinor_propagator_type _r, double _Complex ** _R, spinor_propagator_type _s) {"
+echo "  double _Complex _c;"
+echo "  spinor_propagator_type _sp;"
+echo "  create_sp( &_sp );"
+
+for((s3=0;s3<$N_SPINOR_DIM;s3++)); do
+  for((s2=0;s2<$N_SPINOR_DIM;s2++)); do
+    echo "  _c = 0.;"
+
+    for((s1=0;s1<$N_SPINOR_DIM;s1++)); do
+      echo "  _c += ( (_s)[$(printf "%2d" $s1 )][$(printf "%2d" $((2*$s2)) )] + (_s)[$(printf "%2d" $s1 )][$(printf "%2d" $((2*$s2+1)) )] * I) *  (_R)[$s1][$s3];"
+    done
+    echo "  (_sp)[$(printf "%2d" $s3 )][$(printf "%2d" $((2*$s2))  )] = creal(_c);"
+    echo "  (_sp)[$(printf "%2d" $s3 )][$(printf "%2d" $((2*$s2+1)))] = cimag(_c);"
+  done
+done
+echo "  _sp_eq_sp( _r, _sp);"
+echo "  free_sp( &_sp);"
+echo "}  /* end of rot_sp_ti_bispinor_mat */"
+
+# fi
+
+
+# if [ 0 -eq  1 ]; then
+echo "inline void rot_bispinor_mat_ti_sp ( spinor_propagator_type _r, double _Complex ** _R, spinor_propagator_type _s) {"
+echo "  double _Complex _c;"
+echo "  spinor_propagator_type _sp;"
+echo "  create_sp( &_sp );"
+
+for((s3=0;s3<$N_SPINOR_DIM;s3++)); do
+  for((s2=0;s2<$N_SPINOR_DIM;s2++)); do
+    echo "  _c = 0.;"
+
+    for((s1=0;s1<$N_SPINOR_DIM;s1++)); do
+      echo "  _c += ( (_s)[$(printf "%2d" $s3 )][$(printf "%2d" $((2*$s1)) )] + (_s)[$(printf "%2d" $s3 )][$(printf "%2d" $((2*$s1+1)) )] * I) *  (_R)[$s2][$s1];"
+    done
+    echo "  (_sp)[$(printf "%2d" $s3 )][$(printf "%2d" $((2*$s2))  )] = creal(_c);"
+    echo "  (_sp)[$(printf "%2d" $s3 )][$(printf "%2d" $((2*$s2+1)))] = cimag(_c);"
+  done
+done
+echo "  _sp_eq_sp( _r, _sp);"
+echo "  free_sp( &_sp);"
+echo "}  /* end of rot_bispinor_mat_ti_sp */"
+
+# fi
+
