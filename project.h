869b839125a8b34d83845b893020ac890b065bec
Marcus Petschlies <marcus.petschlies@web.de>
2017-07-05 02:07:27 +0200

    added header for function project_propagator_field_weighted

--- project.h
+++ project.h
@@ -4,7 +4,11 @@
 namespace cvc {
 
 int project_spinor_field(double *s, double * r, int parallel, double *V, int num, unsigned int N);
+
 int project_propagator_field(double *s, double * r, int parallel, double *V, int num1, int num2, unsigned int N);
+
+int project_propagator_field_weighted(double *s, double * r, int parallel, double *V, double *weights, int num1, int num2, unsigned int N);
+
 int project_reduce_from_propagator_field(double *p, double * r, double *V, int num1, int num2, unsigned int N, int xchange);
 int project_expand_to_propagator_field(double *s, double *p, double *V, int num1, int num2, unsigned int N);
 int momentum_projection (double*V, double *W, unsigned int nv, int momentum_number, int (*momentum_list)[3]);
