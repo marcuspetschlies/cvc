e414166068a242022bc07ded2a92d746d96548db
Marcus Petschlies <marcus.petschlies@web.de>
2017-07-14 16:16:53 +0200

    corrections after tests in test_zm4x4.cpp

--- code_scripts/make_zm4x4_code.sh
+++ code_scripts/make_zm4x4_code.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-#for i in 0 1 2 3; do
-#  for k in 0 1 2 3; do
-#    ik=$((4*$i+$k))
-#    ki=$((4*$k+$i))
-#    printf "  r[%2d] = s[%2d];\n" $ik $ki
-#  done
-#done
+if [ 1 -eq 0 ]; then
+for i in 0 1 2 3; do
+  for k in 0 1 2 3; do
+    ik=$((4*$i+$k))
+    ki=$((4*$k+$i))
+    printf "  r[%2d] = s[%2d];\n" $ik $ki
+  done
+done
+fi
 
 if [ 1 -eq 0 ]; then
 for i in 0 1 2 3; do
@@ -28,6 +30,7 @@ for i in 0 1 2 3; do
 done
 fi
 
+# spin-parity projection 1/2 x ( 1 + c gamma_0 ), gamma_0 in tmLQCD basis
 if [ 1 -eq 0 ]; then
 for i1 in 0 1 2 3; do
   k1=$(( ($i1+2)%4 ))
@@ -35,30 +38,44 @@ for i1 in 0 1 2 3; do
     k2=$i2
     ii=$((4*$i1+$i2))
     kk=$((4*$k1+$k2))
-STOPPED HERE
-    printf "  r[0][%2d] = ( b[0][%2d] - b[0][%2d] * c ) 0.5;\n" $ii $ii $kk
+    printf "  r[0][%2d] = ( b[%2d] - b[%2d] * c ) * 0.5;\n" $ii $ii $kk
   done
 done
 fi
 
-if [ 1 -eq 0 ]; then
+# r = s x t + c u
+# if [ 1 -eq 0 ]; then
 for i in 0 1 2 3; do
   for k in 0 1 2 3; do
     ik=$(( 4*$i + $k ))
     l=0
     il=$(( 4*$i + $l ))
     lk=$(( 4*$l + $k ))
-    printf "  r[0][%2d] = ss[%2d] * tt[%2d] + u[0][%2d] * c" $ik  $il $lk $ik
+    printf "  r[0][%2d] = ss[%2d] * tt[%2d] " $ik  $il $lk
     for(( l=1; l<=2; l++)); do
       il=$(( 4*$i + $l ))
       lk=$(( 4*$l + $k ))
-      printf " + ss[%2d] * tt[%2d] + u[0][%2d] * c" $il $lk $ik
+      printf " + ss[%2d] * tt[%2d] " $il $lk
     done
     il=$(( 4*$i + $l ))
     lk=$(( 4*$l + $k ))
     printf " + ss[%2d] * tt[%2d] + u[0][%2d] * c;\n" $il $lk $ik
   done
 done
+# fi
+
+if [ 1 -eq 0 ]; then
+printf "  pp = 0.;\n"
+for i in 0 1 2 3; do
+  for k in 0 1 2 3; do
+    ik=$(( 4*$i + $k ))
+    printf "  c   = r[0][%2d] - s[0][%2d];" $ik $ik
+    printf "  pp += creal( c * conj(c) );\n"
+  done
+done
+printf "  (*p) = sqrt(pp);\n"
 fi
+
+
 
 exit 0
