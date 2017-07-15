3bccb04f9d42a6c390460bb8496fae2fb077d412
Marcus Petschlies <marcus.petschlies@web.de>
2017-07-15 12:57:32 +0200

    tested gi11, gf11 multiplication, source phase multiplication, boundary step function multiplication for B-B type

--- piN2piN_correlators.cpp
+++ piN2piN_correlators.cpp
@@ -154,20 +154,6 @@ int main(int argc, char **argv) {
    ******************************************************/
   init_gamma_matrix ();
 
-  gamma_matrix_type gamma_0, gamma_2, gamma_C, C_gi1;
-
-  gamma_matrix_init ( &C_gi1 );
-
-  gamma_matrix_set ( &gamma_0, 0, 1 );
-  gamma_matrix_set ( &gamma_2, 2, 1 );
-  
-  gamma_matrix_init ( &gamma_C );
-  gamma_matrix_mult ( &gamma_C, &gamma_0, &gamma_2 );
-
-  gamma_matrix_printf (&gamma_0, "g0", stdout);
-  gamma_matrix_printf (&gamma_2, "g2", stdout);
-  gamma_matrix_printf (&gamma_C, "C", stdout);
-
   /******************************************************
    * check source coords list
    ******************************************************/
@@ -186,24 +172,8 @@ int main(int argc, char **argv) {
     int t_base = g_source_coords_list[i_src][0];
 
     if(io_proc == 2) {
-      sprintf(filename, "%s.%.4d.tsrc%.2d.aff", "piN_piN_diagrams", Nconf, t_base );
 
-      fprintf(stdout, "# [piN2piN_correlators] reading data from file %s\n", filename);
-      affr = aff_reader (filename);
-      aff_status_str = (char*)aff_reader_errstr(affr);
-      if( aff_status_str != NULL ) {
-        fprintf(stderr, "[piN2piN_correlators] Error from aff_reader, status was %s\n", aff_status_str);
-        EXIT(4);
-      } else {
-        fprintf(stdout, "# [piN2piN_correlators] reading data from aff file %s\n", filename);
-      }
-
-      if( (affn = aff_reader_root( affr )) == NULL ) {
-        fprintf(stderr, "[piN2piN_correlators] Error, aff writer is not initialized\n");
-        EXIT(103);
-      }
-
-      sprintf(filename, "%s.%.4d.tsrc%.2d.aff", "piN_piN_correlator", Nconf, t_base );
+      sprintf(filename, "%s.%.4d.tsrc%.2d", "piN_piN_correlator", Nconf, t_base );
       ofs = fopen( filename, "w");
       if( ofs == NULL ) {
         fprintf(stderr, "[piN2piN_correlators] Error from fopen \n");
@@ -240,9 +210,9 @@ int main(int argc, char **argv) {
        *******************************************/
       for ( int i2pt = 0; i2pt < g_twopoint_function_number; i2pt++ ) {
 
-        if ( strcmp( g_twopoint_function_list[i2pt].type , "pixN-pixN") != 0 ) continue; 
+        if ( strcmp( g_twopoint_function_list[i2pt].type , "b-b") != 0 ) continue; 
 
-        double _Complex *correlator, ***diagram = NULL, ***diagram_buffer = NULL;
+        double _Complex *correlator = NULL, ***diagram = NULL, ***diagram_buffer = NULL;
 
         exitstatus= init_1level_zbuffer ( &correlator, T_global );
         if ( exitstatus != 0 ) {
@@ -289,15 +259,43 @@ int main(int argc, char **argv) {
         int gf1[2] = { g_twopoint_function_list[i2pt].gf1[0], g_twopoint_function_list[i2pt].gf1[1] };
         int gf2    = g_twopoint_function_list[i2pt].gf2;
 
+
+        /* open file */
+        sprintf(filename, "%s.%.4d.tsrc%.2d.aff", "B_B", Nconf, t_base );
+
+        fprintf(stdout, "# [piN2piN_correlators] reading data from file %s\n", filename);
+        affr = aff_reader (filename);
+        aff_status_str = (char*)aff_reader_errstr(affr);
+        if( aff_status_str != NULL ) {
+          fprintf(stderr, "[piN2piN_correlators] Error from aff_reader, status was %s\n", aff_status_str);
+          EXIT(4);
+        } else {
+          fprintf(stdout, "# [piN2piN_correlators] reading data from aff file %s\n", filename);
+        }
+
+        if( (affn = aff_reader_root( affr )) == NULL ) {
+          fprintf(stderr, "[piN2piN_correlators] Error, aff writer is not initialized\n");
+          EXIT(103);
+        }
+
         char aff_tag[400], aff_tag_suffix[200];
-        sprintf(aff_tag_suffix, "pi2x%.2dpi2y%.2dpi2z%.2d/pf1x%.2dpf1y%.2dpf1z%.2d/pf2x%.2dpf2y%.2dpf2z%.2d/t%.2dx%.2dy%.2dz%.2d/g%.2dg%.2d",
+
+        if ( strcmp(g_twopoint_function_list[i2pt].type, "b-b") == 0 ) {
+          sprintf(aff_tag_suffix, "pf1x%.2dpf1y%.2dpf1z%.2d/t%.2dx%.2dy%.2dz%.2d/g%.2dg%.2d",
+            pf1[0], pf1[1], pf1[2],
+            gsx[0], gsx[1], gsx[2], gsx[3],
+            gf1[0], gi1[0]);
+        }
+        /* sprintf(aff_tag_suffix, "pi2x%.2dpi2y%.2dpi2z%.2d/pf1x%.2dpf1y%.2dpf1z%.2d/pf2x%.2dpf2y%.2dpf2z%.2d/t%.2dx%.2dy%.2dz%.2d/g%.2dg%.2d",
             pi2[0], pi2[1], pi2[2],
             pf1[0], pf1[1], pf1[2],
             pf2[0], pf2[1], pf2[2],
             gsx[0], gsx[1], gsx[2], gsx[3],
-            gf1[0], gi1[0]);
+            gf1[0], gi1[0]); */
 
-        for ( int idiag = 0; idiag < g_twopoint_function_list[i2pt].n; idiag++ ) {
+        for ( int idiag = 0; idiag < g_twopoint_function_list[i2pt].n; idiag++ )
+        // for ( int idiag = 0; idiag < 1; idiag++ )
+        {
 
           if ( io_proc == 2 ) {
             sprintf(aff_tag, "/%s/diag%d/%s", g_twopoint_function_list[i2pt].name, idiag, aff_tag_suffix);
@@ -317,47 +315,71 @@ int main(int argc, char **argv) {
             for ( int it = 0; it < T_global; it++ ) {
               zm_pl_eq_zm_transposed_4x4_array ( diagram[it][0], diagram_buffer[it][0] );
             }
-
           }  /* end of if io_proc == 2 */
+        }   /* end of loop on diagrams */
 
-          /* source phase */
-          exitstatus = correlator_add_source_phase ( diagram, pi1,  &(gsx[1]), T_global );
+        /* TEST */
+        /* for ( int it = 0; it < T; it++ ) {
+          fprintf(stdout, "# initial correlator t = %2d\n", it );
+          zm4x4_printf ( diagram[it], "c_i", stdout );
+        } */
+
+        /* source phase */
+        exitstatus = correlator_add_source_phase ( diagram, pi1,  &(gsx[1]), T_global );
+
+        /* TEST */
+        /* for ( int it = 0; it < T; it++ ) {
+          fprintf( stdout, "# with source phase t = %2d\n", it );
+          zm4x4_printf ( diagram[it], "c_sp", stdout );
+        } */
   
-          /* boundary phase */
-          exitstatus = correlator_add_baryon_boundary_phase ( diagram, gsx[0] );
+        /* boundary phase */
+        exitstatus = correlator_add_baryon_boundary_phase ( diagram, gsx[0] );
 
-          /* aplly gi1[1] and gf1[1] */
-          gamma_matrix_type gf11, gi11;
-          gamma_matrix_set ( &gi11,  gi1[1], 1. );
-          gamma_matrix_set ( &gf11,  gf1[1], 1. );
+        /* TEST */
+        /* for ( int it = 0; it < T; it++ ) {
+          fprintf( stdout, "# with boundary phase t = %2d\n", it );
+          zm4x4_printf ( diagram[it], "c_sp", stdout );
+        } */
+
+        /* aplly gi1[1] and gf1[1] */
+        gamma_matrix_type gf11, gi11;
+        gamma_matrix_set ( &gi11,  gi1[1], 1. );
+        gamma_matrix_set ( &gf11,  gf1[1], 1. );
 
 
 #ifdef HAVE_OPENMP
 #pragma omp parallel for
 #endif
-          for ( int it = 0; it < T_global; it++ ) {
-  
-            /* diagram_buffer <- diagram x Gamma_i1_1 */
-            zm4x4_eq_zm4x4_ti_zm4x4 ( diagram_buffer[it], diagram[it],  gi11.m );
-            /* diagram <- Gamma_f1_1 x diagram_buffer */
-            zm4x4_eq_zm4x4_ti_zm4x4 ( diagram[it], gf11.m, diagram_buffer[it] );
+        for ( int it = 0; it < T_global; it++ ) {
 
-            co_eq_tr_zm4x4 ( correlator+it, diagram[it] );
+          /* diagram_buffer <- diagram x Gamma_i1_1 */
+          zm4x4_eq_zm4x4_ti_zm4x4 ( diagram_buffer[it], diagram[it],  gi11.m );
+          /* diagram <- Gamma_f1_1 x diagram_buffer */
+          zm4x4_eq_zm4x4_ti_zm4x4 ( diagram[it], gf11.m, diagram_buffer[it] );
+          /* spin-trace */
+          co_eq_tr_zm4x4 ( correlator+it, diagram[it] );
+        }
 
-          }
+        /* ??? spin / spin-parity projection ??? */
 
-          /* ??? projection ??? */
+        /* write to file  */
+        sprintf(aff_tag, "%s/pi2x%.2dpi2y%.2dpi2z%.2d/pf1x%.2dpf1y%.2dpf1z%.2d/pf2x%.2dpf2y%.2dpf2z%.2d/t%.2dx%.2dy%.2dz%.2d/g%.2dg%.2d",
+          g_twopoint_function_list[i2pt].name,
+          pi2[0], pi2[1], pi2[2],
+          pf1[0], pf1[1], pf1[2],
+          pf2[0], pf2[1], pf2[2],
+          gsx[0], gsx[1], gsx[2], gsx[3],
+          gf1[0], gi1[0]);
 
-          /* write to file  */
-          for ( int it = 0; it < T_global; it++ ) {
-            fprintf(ofs, "  %25.16e %25.16e\n", creal(correlator[it]), cimag(correlator[it]));
-          }
-
-
-        }   /* end of loop on diagrams */
+        fprintf(ofs, "# %s\n", aff_tag );
+        for ( int it = 0; it < T_global; it++ ) {
+          fprintf(ofs, "  %25.16e %25.16e\n", creal(correlator[it]), cimag(correlator[it]));
+        }
 
         fini_3level_zbuffer ( &diagram_buffer );
         fini_3level_zbuffer ( &diagram );
+        fini_1level_zbuffer ( &correlator );
       }  /* end of loop on 2-point functions */
 
     }  /* end of loop on coherent source locations */
