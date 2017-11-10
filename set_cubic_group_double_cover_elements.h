f0417dcdb299de580ae155117310224c061822d0
Marcus Petschlies <marcus.petschlies@web.de>
2017-11-10 17:22:37 +0100

    include files with code to fill the cubic rotation tables

---
+++ set_cubic_group_double_cover_elements.h
@@ -0,0 +1,240 @@
+  cubic_group_double_cover_rotations[0].n[0] =  0;
+  cubic_group_double_cover_rotations[0].n[1] =  0;
+  cubic_group_double_cover_rotations[0].n[2] =  0;
+  cubic_group_double_cover_rotations[0].w    = 0;
+
+  cubic_group_double_cover_rotations[1].n[0] =  1;
+  cubic_group_double_cover_rotations[1].n[1] =  0;
+  cubic_group_double_cover_rotations[1].n[2] =  0;
+  cubic_group_double_cover_rotations[1].w    = M_PI;
+
+  cubic_group_double_cover_rotations[2].n[0] =  0;
+  cubic_group_double_cover_rotations[2].n[1] =  1;
+  cubic_group_double_cover_rotations[2].n[2] =  0;
+  cubic_group_double_cover_rotations[2].w    = M_PI;
+
+  cubic_group_double_cover_rotations[3].n[0] =  0;
+  cubic_group_double_cover_rotations[3].n[1] =  0;
+  cubic_group_double_cover_rotations[3].n[2] =  1;
+  cubic_group_double_cover_rotations[3].w    = M_PI;
+
+  cubic_group_double_cover_rotations[4].n[0] =  1;
+  cubic_group_double_cover_rotations[4].n[1] =  0;
+  cubic_group_double_cover_rotations[4].n[2] =  0;
+  cubic_group_double_cover_rotations[4].w    = -M_PI;
+
+  cubic_group_double_cover_rotations[5].n[0] =  0;
+  cubic_group_double_cover_rotations[5].n[1] =  1;
+  cubic_group_double_cover_rotations[5].n[2] =  0;
+  cubic_group_double_cover_rotations[5].w    = -M_PI;
+
+  cubic_group_double_cover_rotations[6].n[0] =  0;
+  cubic_group_double_cover_rotations[6].n[1] =  0;
+  cubic_group_double_cover_rotations[6].n[2] =  1;
+  cubic_group_double_cover_rotations[6].w    = -M_PI;
+
+  cubic_group_double_cover_rotations[7].n[0] =  1;
+  cubic_group_double_cover_rotations[7].n[1] =  0;
+  cubic_group_double_cover_rotations[7].n[2] =  0;
+  cubic_group_double_cover_rotations[7].w    = M_PI/2;
+
+  cubic_group_double_cover_rotations[8].n[0] =  0;
+  cubic_group_double_cover_rotations[8].n[1] =  1;
+  cubic_group_double_cover_rotations[8].n[2] =  0;
+  cubic_group_double_cover_rotations[8].w    = M_PI/2;
+
+  cubic_group_double_cover_rotations[9].n[0] =  0;
+  cubic_group_double_cover_rotations[9].n[1] =  0;
+  cubic_group_double_cover_rotations[9].n[2] =  1;
+  cubic_group_double_cover_rotations[9].w    = M_PI/2;
+
+  cubic_group_double_cover_rotations[10].n[0] =  1;
+  cubic_group_double_cover_rotations[10].n[1] =  0;
+  cubic_group_double_cover_rotations[10].n[2] =  0;
+  cubic_group_double_cover_rotations[10].w    = -M_PI/2;
+
+  cubic_group_double_cover_rotations[11].n[0] =  0;
+  cubic_group_double_cover_rotations[11].n[1] =  1;
+  cubic_group_double_cover_rotations[11].n[2] =  0;
+  cubic_group_double_cover_rotations[11].w    = -M_PI/2;
+
+  cubic_group_double_cover_rotations[12].n[0] =  0;
+  cubic_group_double_cover_rotations[12].n[1] =  0;
+  cubic_group_double_cover_rotations[12].n[2] =  1;
+  cubic_group_double_cover_rotations[12].w    = -M_PI/2;
+
+  cubic_group_double_cover_rotations[13].n[0] =  1;
+  cubic_group_double_cover_rotations[13].n[1] =  0;
+  cubic_group_double_cover_rotations[13].n[2] =  0;
+  cubic_group_double_cover_rotations[13].w    = 3*M_PI/2;
+
+  cubic_group_double_cover_rotations[14].n[0] =  0;
+  cubic_group_double_cover_rotations[14].n[1] =  1;
+  cubic_group_double_cover_rotations[14].n[2] =  0;
+  cubic_group_double_cover_rotations[14].w    = 3*M_PI/2;
+
+  cubic_group_double_cover_rotations[15].n[0] =  0;
+  cubic_group_double_cover_rotations[15].n[1] =  0;
+  cubic_group_double_cover_rotations[15].n[2] =  1;
+  cubic_group_double_cover_rotations[15].w    = 3*M_PI/2;
+
+  cubic_group_double_cover_rotations[16].n[0] =  1;
+  cubic_group_double_cover_rotations[16].n[1] =  0;
+  cubic_group_double_cover_rotations[16].n[2] =  0;
+  cubic_group_double_cover_rotations[16].w    = -3*M_PI/2;
+
+  cubic_group_double_cover_rotations[17].n[0] =  0;
+  cubic_group_double_cover_rotations[17].n[1] =  1;
+  cubic_group_double_cover_rotations[17].n[2] =  0;
+  cubic_group_double_cover_rotations[17].w    = -3*M_PI/2;
+
+  cubic_group_double_cover_rotations[18].n[0] =  0;
+  cubic_group_double_cover_rotations[18].n[1] =  0;
+  cubic_group_double_cover_rotations[18].n[2] =  1;
+  cubic_group_double_cover_rotations[18].w    = -3*M_PI/2;
+
+  cubic_group_double_cover_rotations[19].n[0] =  1;
+  cubic_group_double_cover_rotations[19].n[1] =  1;
+  cubic_group_double_cover_rotations[19].n[2] =  1;
+  cubic_group_double_cover_rotations[19].w    = 2*M_PI/3;
+
+  cubic_group_double_cover_rotations[20].n[0] = -1;
+  cubic_group_double_cover_rotations[20].n[1] =  1;
+  cubic_group_double_cover_rotations[20].n[2] =  1;
+  cubic_group_double_cover_rotations[20].w    = 2*M_PI/3;
+
+  cubic_group_double_cover_rotations[21].n[0] = -1;
+  cubic_group_double_cover_rotations[21].n[1] = -1;
+  cubic_group_double_cover_rotations[21].n[2] =  1;
+  cubic_group_double_cover_rotations[21].w    = 2*M_PI/3;
+
+  cubic_group_double_cover_rotations[22].n[0] =  1;
+  cubic_group_double_cover_rotations[22].n[1] = -1;
+  cubic_group_double_cover_rotations[22].n[2] =  1;
+  cubic_group_double_cover_rotations[22].w    = 2*M_PI/3;
+
+  cubic_group_double_cover_rotations[23].n[0] =  1;
+  cubic_group_double_cover_rotations[23].n[1] =  1;
+  cubic_group_double_cover_rotations[23].n[2] =  1;
+  cubic_group_double_cover_rotations[23].w    = -2*M_PI/3;
+
+  cubic_group_double_cover_rotations[24].n[0] = -1;
+  cubic_group_double_cover_rotations[24].n[1] =  1;
+  cubic_group_double_cover_rotations[24].n[2] =  1;
+  cubic_group_double_cover_rotations[24].w    = -2*M_PI/3;
+
+  cubic_group_double_cover_rotations[25].n[0] = -1;
+  cubic_group_double_cover_rotations[25].n[1] = -1;
+  cubic_group_double_cover_rotations[25].n[2] =  1;
+  cubic_group_double_cover_rotations[25].w    = -2*M_PI/3;
+
+  cubic_group_double_cover_rotations[26].n[0] =  1;
+  cubic_group_double_cover_rotations[26].n[1] = -1;
+  cubic_group_double_cover_rotations[26].n[2] =  1;
+  cubic_group_double_cover_rotations[26].w    = -2*M_PI/3;
+
+  cubic_group_double_cover_rotations[27].n[0] =  1;
+  cubic_group_double_cover_rotations[27].n[1] =  1;
+  cubic_group_double_cover_rotations[27].n[2] =  1;
+  cubic_group_double_cover_rotations[27].w    = 4*M_PI/3;
+
+  cubic_group_double_cover_rotations[28].n[0] = -1;
+  cubic_group_double_cover_rotations[28].n[1] =  1;
+  cubic_group_double_cover_rotations[28].n[2] =  1;
+  cubic_group_double_cover_rotations[28].w    = 4*M_PI/3;
+
+  cubic_group_double_cover_rotations[29].n[0] = -1;
+  cubic_group_double_cover_rotations[29].n[1] = -1;
+  cubic_group_double_cover_rotations[29].n[2] =  1;
+  cubic_group_double_cover_rotations[29].w    = 4*M_PI/3;
+
+  cubic_group_double_cover_rotations[30].n[0] =  1;
+  cubic_group_double_cover_rotations[30].n[1] = -1;
+  cubic_group_double_cover_rotations[30].n[2] =  1;
+  cubic_group_double_cover_rotations[30].w    = 4*M_PI/3;
+
+  cubic_group_double_cover_rotations[31].n[0] =  1;
+  cubic_group_double_cover_rotations[31].n[1] =  1;
+  cubic_group_double_cover_rotations[31].n[2] =  1;
+  cubic_group_double_cover_rotations[31].w    = -4*M_PI/3;
+
+  cubic_group_double_cover_rotations[32].n[0] = -1;
+  cubic_group_double_cover_rotations[32].n[1] =  1;
+  cubic_group_double_cover_rotations[32].n[2] =  1;
+  cubic_group_double_cover_rotations[32].w    = -4*M_PI/3;
+
+  cubic_group_double_cover_rotations[33].n[0] = -1;
+  cubic_group_double_cover_rotations[33].n[1] = -1;
+  cubic_group_double_cover_rotations[33].n[2] =  1;
+  cubic_group_double_cover_rotations[33].w    = -4*M_PI/3;
+
+  cubic_group_double_cover_rotations[34].n[0] =  1;
+  cubic_group_double_cover_rotations[34].n[1] = -1;
+  cubic_group_double_cover_rotations[34].n[2] =  1;
+  cubic_group_double_cover_rotations[34].w    = -4*M_PI/3;
+
+  cubic_group_double_cover_rotations[35].n[0] =  0;
+  cubic_group_double_cover_rotations[35].n[1] =  1;
+  cubic_group_double_cover_rotations[35].n[2] =  1;
+  cubic_group_double_cover_rotations[35].w    = M_PI;
+
+  cubic_group_double_cover_rotations[36].n[0] =  0;
+  cubic_group_double_cover_rotations[36].n[1] = -1;
+  cubic_group_double_cover_rotations[36].n[2] =  1;
+  cubic_group_double_cover_rotations[36].w    = M_PI;
+
+  cubic_group_double_cover_rotations[37].n[0] =  1;
+  cubic_group_double_cover_rotations[37].n[1] =  1;
+  cubic_group_double_cover_rotations[37].n[2] =  0;
+  cubic_group_double_cover_rotations[37].w    = M_PI;
+
+  cubic_group_double_cover_rotations[38].n[0] =  1;
+  cubic_group_double_cover_rotations[38].n[1] = -1;
+  cubic_group_double_cover_rotations[38].n[2] =  0;
+  cubic_group_double_cover_rotations[38].w    = M_PI;
+
+  cubic_group_double_cover_rotations[39].n[0] =  1;
+  cubic_group_double_cover_rotations[39].n[1] =  0;
+  cubic_group_double_cover_rotations[39].n[2] =  1;
+  cubic_group_double_cover_rotations[39].w    = M_PI;
+
+  cubic_group_double_cover_rotations[40].n[0] = -1;
+  cubic_group_double_cover_rotations[40].n[1] =  0;
+  cubic_group_double_cover_rotations[40].n[2] =  1;
+  cubic_group_double_cover_rotations[40].w    = M_PI;
+
+  cubic_group_double_cover_rotations[41].n[0] =  0;
+  cubic_group_double_cover_rotations[41].n[1] =  1;
+  cubic_group_double_cover_rotations[41].n[2] =  1;
+  cubic_group_double_cover_rotations[41].w    = -M_PI;
+
+  cubic_group_double_cover_rotations[42].n[0] =  0;
+  cubic_group_double_cover_rotations[42].n[1] = -1;
+  cubic_group_double_cover_rotations[42].n[2] =  1;
+  cubic_group_double_cover_rotations[42].w    = -M_PI;
+
+  cubic_group_double_cover_rotations[43].n[0] =  1;
+  cubic_group_double_cover_rotations[43].n[1] =  1;
+  cubic_group_double_cover_rotations[43].n[2] =  0;
+  cubic_group_double_cover_rotations[43].w    = -M_PI;
+
+  cubic_group_double_cover_rotations[44].n[0] =  1;
+  cubic_group_double_cover_rotations[44].n[1] = -1;
+  cubic_group_double_cover_rotations[44].n[2] =  0;
+  cubic_group_double_cover_rotations[44].w    = -M_PI;
+
+  cubic_group_double_cover_rotations[45].n[0] =  1;
+  cubic_group_double_cover_rotations[45].n[1] =  0;
+  cubic_group_double_cover_rotations[45].n[2] =  1;
+  cubic_group_double_cover_rotations[45].w    = -M_PI;
+
+  cubic_group_double_cover_rotations[46].n[0] = -1;
+  cubic_group_double_cover_rotations[46].n[1] =  0;
+  cubic_group_double_cover_rotations[46].n[2] =  1;
+  cubic_group_double_cover_rotations[46].w    = -M_PI;
+
+  cubic_group_double_cover_rotations[47].n[0] =  0;
+  cubic_group_double_cover_rotations[47].n[1] =  0;
+  cubic_group_double_cover_rotations[47].n[2] =  0;
+  cubic_group_double_cover_rotations[47].w    = 2*M_PI;
+
