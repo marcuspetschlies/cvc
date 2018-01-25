f3269f702fd9a7b756f7313c5874da0f79d503d3
Marcus <marcus.petschlies@web.de>
2018-01-25 18:57:02 +0100

    added new init functions; changed all EXIT to return

--- matrix_init.h
+++ matrix_init.h
@@ -34,6 +34,16 @@ int init_4level_buffer (double*****buffer, unsigned int n1, unsigned int n2, uns
 
 int fini_4level_buffer (double*****buffer);
 
+/************************************************************************************/
+/************************************************************************************/
+
+/************************************************************************************
+ * (de-)allocate 1-level zbuffer n1 vector
+ ************************************************************************************/
+int init_1level_zbuffer (double _Complex**zbuffer, unsigned int n1 );
+
+int fini_1level_zbuffer (double _Complex**zbuffer);
+
 
 /************************************************************************************
  * (de-)allocate 2-level zbuffer (n1 x n2 matrix)
@@ -41,6 +51,14 @@ int fini_4level_buffer (double*****buffer);
 int init_2level_zbuffer (double _Complex***zbuffer, unsigned int n1, unsigned int n2);
 
 int fini_2level_zbuffer (double _Complex***zbuffer);
+
+/************************************************************************************
+ * (de-)allocate 2-level zbuffer ( asymmetric )
+ ************************************************************************************/
+int init_2level_zbuffer_asym (double _Complex***zbuffer, int n, int *dim);
+
+int fini_2level_zbuffer_asym (double _Complex***zbuffer );
+
 
 /************************************************************************************
  * (de-)allocate 3-level zbuffer (n1 x n2 x n3 matrix)
@@ -63,6 +81,10 @@ int fini_4level_zbuffer (double _Complex*****zbuffer);
 /************************************************************************************
  * integer matrices
  ************************************************************************************/
+
+int init_1level_ibuffer ( int**buffer, unsigned int n1 );
+int fini_1level_ibuffer ( int**buffer );
+
 
 int init_2level_ibuffer ( int***buffer, unsigned int n1, unsigned int n2);
 int fini_2level_ibuffer ( int***buffer );
