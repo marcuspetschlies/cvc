e50de4a45f942077c438af24133bd497d453c276
Marcus <marcus.petschlies@web.de>
2018-03-09 11:18:30 +0100

    added Oh irrep matrix settings

--- set_rot_mat_table_cubic_group_single_cover_v2.cpp
+++ set_rot_mat_table_cubic_group_single_cover_v2.cpp
@@ -20,7 +20,7 @@ int set_rot_mat_table_cubic_group_single_cover ( rot_mat_table_type *t, char *gr
     /***********************************************************
      * LG Oh irrep A1
      ***********************************************************/
-    if ( strcmp ( irrep, "A1" ) == 0 ) {
+    if ( strcmp ( irrep, "A1g" ) == 0 ) {
       if ( ( exitstatus = alloc_rot_mat_table ( t, group, irrep, 1, nrot) ) != 0 ) {
         fprintf(stderr, "[set_rot_mat_table_cubic_group_single_cover] Error from alloc_rot_mat_table, status was %d %s %d\n", exitstatus, __FILE__, __LINE__);
         return(1);
@@ -29,14 +29,32 @@ int set_rot_mat_table_cubic_group_single_cover ( rot_mat_table_type *t, char *gr
       for ( int i = 0; i < nrot; i++ ) {
         t->rid[i] = i;
         t->rmid[i] = i;
-        t->R[i][0][0]  = 1.;
-        t->IR[i][0][0] = 1.;
       }
-#if 0
+      double _Complex ***R = t->R;
+      double _Complex ***IR = t->IR;
+#include "./irrep_matrices/set_rot_mat_table_cubic_group_single_cover_Oh_A1g_v2.cpp"
+
     /***********************************************************
-     * LG Oh irrep A2
+     * LG Oh irrep A1u
      ***********************************************************/
-    } else if ( strcmp ( irrep, "A2" ) == 0 ) {
+    } else if ( strcmp ( irrep, "A1u" ) == 0 ) {
+      if ( ( exitstatus = alloc_rot_mat_table ( t, group, irrep, 1, nrot) ) != 0 ) {
+        fprintf(stderr, "[set_rot_mat_table_cubic_group_single_cover] Error from alloc_rot_mat_table, status was %d %s %d\n", exitstatus, __FILE__, __LINE__);
+        return(1);
+      }
+
+      for ( int i = 0; i < nrot; i++ ) {
+        t->rid[i] = i;
+        t->rmid[i] = i;
+      }
+      double _Complex ***R = t->R;
+      double _Complex ***IR = t->IR;
+#include "./irrep_matrices/set_rot_mat_table_cubic_group_single_cover_Oh_A1u_v2.cpp"
+
+    /***********************************************************
+     * LG Oh irrep A2g
+     ***********************************************************/
+    } else if ( strcmp ( irrep, "A2g" ) == 0 ) {
       if ( ( exitstatus = alloc_rot_mat_table ( t, group, irrep, 1, nrot) ) != 0 ) {
         fprintf(stderr, "[set_rot_mat_table_cubic_group_single_cover] Error from alloc_rot_mat_table, status was %d %s %d\n", exitstatus, __FILE__, __LINE__);
         return(1);
@@ -45,163 +63,68 @@ int set_rot_mat_table_cubic_group_single_cover ( rot_mat_table_type *t, char *gr
       for ( int i = 0; i < nrot; i++ ) { 
         t->rid[i]      = i; 
         t->rmid[i]     = i; 
-        t->R[i][0][0]  = 1.;
-        t->IR[i][0][0] = 1.;
       }
+      double _Complex ***R = t->R;
+      double _Complex ***IR = t->IR;
+#include "./irrep_matrices/set_rot_mat_table_cubic_group_single_cover_Oh_A2g_v2.cpp"
 
-      /* 6C8', 6C8 */
-      for ( int i = 7; i <= 18; i++ ) {
-        t->R[i][0][0]  = -1.;
-        t->IR[i][0][0] = -1.;
-      }
-     
-      /* 12C4' */
-      for ( int i = 35; i <= 46; i++ ) {
-        t->R[i][0][0]  = -1.;
-        t->IR[i][0][0] = -1.;
-      }
-#endif
-
-#if 0
     /***********************************************************
-     * LG Oh irrep E
+     * LG Oh irrep A2u
      ***********************************************************/
-    } else if ( strcmp ( irrep, "E" ) == 0 ) {
+    } else if ( strcmp ( irrep, "A2u" ) == 0 ) {
+      if ( ( exitstatus = alloc_rot_mat_table ( t, group, irrep, 1, nrot) ) != 0 ) {
+        fprintf(stderr, "[set_rot_mat_table_cubic_group_single_cover] Error from alloc_rot_mat_table, status was %d %s %d\n", exitstatus, __FILE__, __LINE__);
+        return(1);
+      }
+
+      for ( int i = 0; i < nrot; i++ ) { 
+        t->rid[i]      = i; 
+        t->rmid[i]     = i; 
+      }
+      double _Complex ***R = t->R;
+      double _Complex ***IR = t->IR;
+#include "./irrep_matrices/set_rot_mat_table_cubic_group_single_cover_Oh_A2u_v2.cpp"
+
+    /***********************************************************
+     * LG Oh irrep Eg
+     ***********************************************************/
+    } else if ( strcmp ( irrep, "Eg" ) == 0 ) {
 
       if ( ( exitstatus = alloc_rot_mat_table ( t, group, irrep, 2, nrot ) ) != 0 ) {
         fprintf(stderr, "[set_rot_mat_table_cubic_group_single_cover] Error from alloc_rot_mat_table, status was %d %s %d\n", exitstatus, __FILE__, __LINE__);
         return(1);
       }
 
-      for ( int i = 0; i < nrot; i++ ) { t->rid[i] = i; }
+      for ( int i = 0; i < nrot; i++ ) { 
+        t->rid[i]  = i; 
+        t->rmid[i] = i; 
+      }
+      double _Complex ***R = t->R;
+      double _Complex ***IR = t->IR;
+#include "./irrep_matrices/set_rot_mat_table_cubic_group_single_cover_Oh_Eg_v2.cpp"
 
-      /* R1, R48 */
-      t->R[ 0][0][0] = 1.; t->R[ 0][1][1] = 1.;
-      t->R[47][0][0] = 1.; t->R[47][1][1] = 1.;
-      /* R2, R5 */
-      t->R[ 1][0][0] = 1.; t->R[ 1][1][1] = 1.;
-      t->R[ 4][0][0] = 1.; t->R[ 4][1][1] = 1.;
-      /* R3, R6 */
-      t->R[ 2][0][0] = 1.; t->R[ 2][1][1] = 1.;
-      t->R[ 5][0][0] = 1.; t->R[ 5][1][1] = 1.;
-      /* R4, R7 */
-      t->R[ 3][0][0] = 1.; t->R[ 3][1][1] = 1.;
-      t->R[ 6][0][0] = 1.; t->R[ 6][1][1] = 1.;
-
-      /* R13, R16 sigma_3 */
-      t->R[12][0][0] = 1.; t->R[12][1][1] = -1.;
-      t->R[15][0][0] = 1.; t->R[15][1][1] = -1.;
-      /* R10, R19 sigma_3 */
-      t->R[ 9][0][0] = 1.; t->R[ 9][1][1] = -1.;
-      t->R[18][0][0] = 1.; t->R[18][1][1] = -1.;
-      /* R38, R44 sigma_3 */
-      t->R[37][0][0] = 1.; t->R[37][1][1] = -1.;
-      t->R[43][0][0] = 1.; t->R[43][1][1] = -1.;
-      /* R39, R45 sigma_3 */
-      t->R[38][0][0] = 1.; t->R[38][1][1] = -1.;
-      t->R[44][0][0] = 1.; t->R[44][1][1] = -1.;
-      
-      /* -1/2 1 + i sqrt(3)/2 sigma_2 */
-
-      /* R24, R28 */
-      t->R[23][0][0] = -ONE_HALF;   t->R[23][1][1] = -ONE_HALF;
-      t->R[23][0][1] =  SQRT3_HALF; t->R[23][1][0] = -SQRT3_HALF;
-      t->R[27][0][0] = -ONE_HALF;   t->R[27][1][1] = -ONE_HALF;
-      t->R[27][0][1] =  SQRT3_HALF; t->R[27][1][0] = -SQRT3_HALF;
-      /* R21, R33 */
-      t->R[20][0][0] = -ONE_HALF;   t->R[20][1][1] = -ONE_HALF;
-      t->R[20][0][1] =  SQRT3_HALF; t->R[20][1][0] = -SQRT3_HALF;
-      t->R[32][0][0] = -ONE_HALF;   t->R[32][1][1] = -ONE_HALF;
-      t->R[32][0][1] =  SQRT3_HALF; t->R[32][1][0] = -SQRT3_HALF;
-      /* R26, R30 */
-      t->R[25][0][0] = -ONE_HALF;   t->R[25][1][1] = -ONE_HALF;
-      t->R[25][0][1] =  SQRT3_HALF; t->R[25][1][0] = -SQRT3_HALF;
-      t->R[29][0][0] = -ONE_HALF;   t->R[29][1][1] = -ONE_HALF;
-      t->R[29][0][1] =  SQRT3_HALF; t->R[29][1][0] = -SQRT3_HALF;
-      /* R23, R35 */
-      t->R[22][0][0] = -ONE_HALF;   t->R[22][1][1] = -ONE_HALF;
-      t->R[22][0][1] =  SQRT3_HALF; t->R[22][1][0] = -SQRT3_HALF;
-      t->R[34][0][0] = -ONE_HALF;   t->R[34][1][1] = -ONE_HALF;
-      t->R[34][0][1] =  SQRT3_HALF; t->R[34][1][0] = -SQRT3_HALF;
-   
-      /* -1/2 1 - i sqrt(3)/2 sigma_2 */
-
-      /* R20, R32 */
-      t->R[19][0][0] = -ONE_HALF;   t->R[19][1][1] = -ONE_HALF;
-      t->R[19][0][1] = -SQRT3_HALF; t->R[19][1][0] =  SQRT3_HALF;
-      t->R[31][0][0] = -ONE_HALF;   t->R[31][1][1] = -ONE_HALF;
-      t->R[31][0][1] = -SQRT3_HALF; t->R[31][1][0] =  SQRT3_HALF;
-      /* R25, R29 */
-      t->R[24][0][0] = -ONE_HALF;   t->R[24][1][1] = -ONE_HALF;
-      t->R[24][0][1] = -SQRT3_HALF; t->R[24][1][0] =  SQRT3_HALF;
-      t->R[28][0][0] = -ONE_HALF;   t->R[28][1][1] = -ONE_HALF;
-      t->R[28][0][1] = -SQRT3_HALF; t->R[28][1][0] =  SQRT3_HALF;
-      /* R22, R34 */
-      t->R[21][0][0] = -ONE_HALF;   t->R[21][1][1] = -ONE_HALF;
-      t->R[21][0][1] = -SQRT3_HALF; t->R[21][1][0] =  SQRT3_HALF;
-      t->R[33][0][0] = -ONE_HALF;   t->R[33][1][1] = -ONE_HALF;
-      t->R[33][0][1] = -SQRT3_HALF; t->R[33][1][0] =  SQRT3_HALF;
-      /* R27, R31 */
-      t->R[26][0][0] = -ONE_HALF;   t->R[26][1][1] = -ONE_HALF;
-      t->R[26][0][1] = -SQRT3_HALF; t->R[26][1][0] =  SQRT3_HALF;
-      t->R[30][0][0] = -ONE_HALF;   t->R[30][1][1] = -ONE_HALF;
-      t->R[30][0][1] = -SQRT3_HALF; t->R[30][1][0] =  SQRT3_HALF;
-
-      /* -cos(pi/3) sigma_3 - sin(pi/3) sigma_1 */
-
-      /* R11, R14 */
-      t->R[10][0][0] = -ONE_HALF;   t->R[10][1][1] =  ONE_HALF;
-      t->R[10][0][1] = -SQRT3_HALF; t->R[10][1][0] = -SQRT3_HALF;
-      t->R[13][0][0] = -ONE_HALF;   t->R[13][1][1] =  ONE_HALF;
-      t->R[13][0][1] = -SQRT3_HALF; t->R[13][1][0] = -SQRT3_HALF;
-      /* R8, R17 */
-      t->R[ 7][0][0] = -ONE_HALF;   t->R[ 7][1][1] =  ONE_HALF;
-      t->R[ 7][0][1] = -SQRT3_HALF; t->R[ 7][1][0] = -SQRT3_HALF;
-      t->R[16][0][0] = -ONE_HALF;   t->R[16][1][1] =  ONE_HALF;
-      t->R[16][0][1] = -SQRT3_HALF; t->R[16][1][0] = -SQRT3_HALF;
-      /* R36, R42 */
-      t->R[35][0][0] = -ONE_HALF;   t->R[35][1][1] =  ONE_HALF;
-      t->R[35][0][1] = -SQRT3_HALF; t->R[35][1][0] = -SQRT3_HALF;
-      t->R[41][0][0] = -ONE_HALF;   t->R[41][1][1] =  ONE_HALF;
-      t->R[41][0][1] = -SQRT3_HALF; t->R[41][1][0] = -SQRT3_HALF;
-      /* R37, R43 */
-      t->R[36][0][0] = -ONE_HALF;   t->R[36][1][1] =  ONE_HALF;
-      t->R[36][0][1] = -SQRT3_HALF; t->R[36][1][0] = -SQRT3_HALF;
-      t->R[42][0][0] = -ONE_HALF;   t->R[42][1][1] =  ONE_HALF;
-      t->R[42][0][1] = -SQRT3_HALF; t->R[42][1][0] = -SQRT3_HALF;
-
-      /* -cos(pi/3) sigma_3 + sin(pi/3) sigma_1 */
-
-      /* R12, R15 */
-      t->R[11][0][0] = -ONE_HALF;   t->R[11][1][1] =  ONE_HALF;
-      t->R[11][0][1] =  SQRT3_HALF; t->R[11][1][0] =  SQRT3_HALF;
-      t->R[14][0][0] = -ONE_HALF;   t->R[14][1][1] =  ONE_HALF;
-      t->R[14][0][1] =  SQRT3_HALF; t->R[14][1][0] =  SQRT3_HALF;
-      /* R9, R18 */
-      t->R[ 8][0][0] = -ONE_HALF;   t->R[ 8][1][1] =  ONE_HALF;
-      t->R[ 8][0][1] =  SQRT3_HALF; t->R[ 8][1][0] =  SQRT3_HALF;
-      t->R[17][0][0] = -ONE_HALF;   t->R[17][1][1] =  ONE_HALF;
-      t->R[17][0][1] =  SQRT3_HALF; t->R[17][1][0] =  SQRT3_HALF;
-      /* R40, R46 */
-      t->R[39][0][0] = -ONE_HALF;   t->R[39][1][1] =  ONE_HALF;
-      t->R[39][0][1] =  SQRT3_HALF; t->R[39][1][0] =  SQRT3_HALF;
-      t->R[45][0][0] = -ONE_HALF;   t->R[45][1][1] =  ONE_HALF;
-      t->R[45][0][1] =  SQRT3_HALF; t->R[45][1][0] =  SQRT3_HALF;
-      /* R41, R47 */
-      t->R[40][0][0] = -ONE_HALF;   t->R[40][1][1] =  ONE_HALF;
-      t->R[40][0][1] =  SQRT3_HALF; t->R[40][1][0] =  SQRT3_HALF;
-      t->R[46][0][0] = -ONE_HALF;   t->R[46][1][1] =  ONE_HALF;
-      t->R[46][0][1] =  SQRT3_HALF; t->R[46][1][0] =  SQRT3_HALF;
-
-      memcpy ( t->rmid, t->rid, nrot * sizeof(int) );
-      memcpy ( t->IR[0][0], t->R[0][0], nrot * 4 * sizeof(double _Complex ) );
-#endif
-
-#if 0
     /***********************************************************
-     * LG Oh irrep T1
+     * LG Oh irrep Eu
      ***********************************************************/
-    } else if ( strcmp ( irrep, "T1" ) == 0 ) {
+    } else if ( strcmp ( irrep, "Eu" ) == 0 ) {
+
+      if ( ( exitstatus = alloc_rot_mat_table ( t, group, irrep, 2, nrot ) ) != 0 ) {
+        fprintf(stderr, "[set_rot_mat_table_cubic_group_single_cover] Error from alloc_rot_mat_table, status was %d %s %d\n", exitstatus, __FILE__, __LINE__);
+        return(1);
+      }
+
+      for ( int i = 0; i < nrot; i++ ) { 
+        t->rid[i]  = i; 
+        t->rmid[i] = i; 
+      }
+      double _Complex ***R = t->R;
+      double _Complex ***IR = t->IR;
+#include "./irrep_matrices/set_rot_mat_table_cubic_group_single_cover_Oh_Eu_v2.cpp"
+
+    /***********************************************************
+     * LG Oh irrep T1g
+     ***********************************************************/
+    } else if ( strcmp ( irrep, "T1g" ) == 0 ) {
 
       if ( ( exitstatus = alloc_rot_mat_table ( t, group, irrep, 3, nrot ) ) != 0 ) {
         fprintf(stderr, "[set_rot_mat_table_cubic_group_single_cover] Error from alloc_rot_mat_table, status was %d %s %d\n", exitstatus, __FILE__, __LINE__);
@@ -209,19 +132,17 @@ int set_rot_mat_table_cubic_group_single_cover ( rot_mat_table_type *t, char *gr
       }
 
       for ( int i = 0; i < nrot; i++ ) {
-        t->rid[i] = i;
-        rot_rotation_matrix_spherical_basis ( t->R[i], 2, cubic_group_double_cover_rotations[i].n, cubic_group_double_cover_rotations[i].w );
+        t->rid[i]  = i;
+        t->rmid[i] = i;
       }
-      
-      memcpy ( t->rmid, t->rid, nrot * sizeof(int) );
-      memcpy ( t->IR[0][0], t->R[0][0], nrot*9*sizeof(double _Complex ) );
-#endif
+      double _Complex ***R = t->R;
+      double _Complex ***IR = t->IR;
+#include "./irrep_matrices/set_rot_mat_table_cubic_group_single_cover_Oh_T1g_v2.cpp"
 
-#if 0
     /***********************************************************
-     * LG Oh irrep T2
+     * LG Oh irrep T1u
      ***********************************************************/
-    } else if ( strcmp ( irrep, "T2" ) == 0 ) {
+    } else if ( strcmp ( irrep, "T1u" ) == 0 ) {
 
       if ( ( exitstatus = alloc_rot_mat_table ( t, group, irrep, 3, nrot ) ) != 0 ) {
         fprintf(stderr, "[set_rot_mat_table_cubic_group_single_cover] Error from alloc_rot_mat_table, status was %d %s %d\n", exitstatus, __FILE__, __LINE__);
@@ -229,19 +150,48 @@ int set_rot_mat_table_cubic_group_single_cover ( rot_mat_table_type *t, char *gr
       }
 
       for ( int i = 0; i < nrot; i++ ) {
-        t->rid[i] = i;
-        rot_rotation_matrix_spherical_basis ( t->R[i], 2, cubic_group_double_cover_rotations[i].n, cubic_group_double_cover_rotations[i].w );
+        t->rid[i]  = i;
+        t->rmid[i] = i;
       }
-  
-      /* 6C8, 6C8' additional minus sign, R8 to R19 */
-      for ( int i =  7; i <= 18; i++ ) { rot_mat_ti_eq_re ( t->R[i], -1., 3 ); }
+      double _Complex ***R = t->R;
+      double _Complex ***IR = t->IR;
+#include "./irrep_matrices/set_rot_mat_table_cubic_group_single_cover_Oh_T1u_v2.cpp"
+      
+    /***********************************************************
+     * LG Oh irrep T2g
+     ***********************************************************/
+    } else if ( strcmp ( irrep, "T2g" ) == 0 ) {
 
-      /* 12C4' additional minus sign, R36 to R47 */
-      for ( int i = 35; i <= 46; i++ ) { rot_mat_ti_eq_re ( t->R[i], -1., 3 ); }
-          
-      memcpy ( t->rmid, t->rid, nrot * sizeof( int ) );
-      memcpy ( t->IR[0][0], t->R[0][0], nrot * 9 * sizeof(double _Complex ) );
-#endif
+      if ( ( exitstatus = alloc_rot_mat_table ( t, group, irrep, 3, nrot ) ) != 0 ) {
+        fprintf(stderr, "[set_rot_mat_table_cubic_group_single_cover] Error from alloc_rot_mat_table, status was %d %s %d\n", exitstatus, __FILE__, __LINE__);
+        return(1);
+      }
+
+      for ( int i = 0; i < nrot; i++ ) {
+        t->rid[i]  = i;
+        t->rmid[i] = i;
+      }
+      double _Complex ***R = t->R;
+      double _Complex ***IR = t->IR;
+#include "./irrep_matrices/set_rot_mat_table_cubic_group_single_cover_Oh_T2g_v2.cpp"
+
+    /***********************************************************
+     * LG Oh irrep T2u
+     ***********************************************************/
+    } else if ( strcmp ( irrep, "T2u" ) == 0 ) {
+
+      if ( ( exitstatus = alloc_rot_mat_table ( t, group, irrep, 3, nrot ) ) != 0 ) {
+        fprintf(stderr, "[set_rot_mat_table_cubic_group_single_cover] Error from alloc_rot_mat_table, status was %d %s %d\n", exitstatus, __FILE__, __LINE__);
+        return(1);
+      }
+
+      for ( int i = 0; i < nrot; i++ ) {
+        t->rid[i]  = i;
+        t->rmid[i] = i;
+      }
+      double _Complex ***R = t->R;
+      double _Complex ***IR = t->IR;
+#include "./irrep_matrices/set_rot_mat_table_cubic_group_single_cover_Oh_T2u_v2.cpp"
 
     } else {
       fprintf(stderr, "[set_rot_mat_table_cubic_group_single_cover] unknown irrep name %s\n", irrep );
