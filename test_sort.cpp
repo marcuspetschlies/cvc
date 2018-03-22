f5e75091283a2b5e8467e4d2c0256d88b615463a
Marcus <marcus.petschlies@web.de>
2018-03-22 07:18:09 +0100

    initial commit of test program for sorting eigenvalues

---
+++ test_sort.cpp
@@ -0,0 +1,187 @@
+/****************************************************
+ * test_sort.cpp
+ *
+ * So 5. Jun 18:15:21 CEST 2016
+ *
+ * PURPOSE:
+ * TODO:
+ * DONE:
+ * CHANGES:
+ ****************************************************/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+#include <complex.h>
+#include <getopt.h>
+#ifdef HAVE_MPI
+#  include <mpi.h>
+#endif
+#ifdef HAVE_OPENMP
+#include <omp.h>
+#endif
+
+#define MAIN_PROGRAM
+
+#include "types.h"
+#include "cvc_complex.h"
+#include "ilinalg.h"
+#include "global.h"
+#include "cvc_geometry.h"
+#include "cvc_utils.h"
+#include "set_default.h"
+#include "mpi_init.h"
+#include "io.h"
+#include "io_utils.h"
+#include "propagator_io.h"
+#include "gauge_io.h"
+#include "read_input_parser.h"
+#include "laplace_linalg.h"
+#include "hyp_smear.h"
+#include "Q_phi.h"
+#include "scalar_products.h"
+#include "ranlxd.h"
+#include "table_init_d.h"
+#include "table_init_z.h"
+
+using namespace cvc;
+
+
+void usage(void) {
+  fprintf(stdout, "usage:\n");
+  exit(0);
+}
+
+int main(int argc, char **argv) {
+  
+  int c, exitstatus; 
+  int filename_set = 0;
+  char filename[200];
+
+  double ratime, retime;
+
+#ifdef HAVE_MPI
+  MPI_Init(&argc, &argv);
+#endif
+
+  while ((c = getopt(argc, argv, "h?vf:")) != -1) {
+    switch (c) {
+    case 'v':
+      g_verbose = 1;
+      break;
+    case 'f':
+      strcpy(filename, optarg);
+      filename_set=1;
+      break;
+    case 'h':
+    case '?':
+    default:
+      usage();
+      break;
+    }
+  }
+
+  /* set the default values */
+  if(filename_set==0) strcpy(filename, "cvc.input");
+  if(g_cart_id==0) fprintf(stdout, "# [test_sort] Reading input from file %s\n", filename);
+  read_input_parser(filename);
+
+#ifdef HAVE_TMLQCD_LIBWRAPPER
+
+  fprintf(stdout, "# [p2gg_xspace] calling tmLQCD wrapper init functions\n");
+
+  /*********************************
+   * initialize MPI parameters for cvc
+   *********************************/
+  exitstatus = tmLQCD_invert_init(argc, argv, 1);
+  if(exitstatus != 0) {
+    EXIT(14);
+  }
+  exitstatus = tmLQCD_get_mpi_params(&g_tmLQCD_mpi);
+  if(exitstatus != 0) {
+    EXIT(15);
+  }
+  exitstatus = tmLQCD_get_lat_params(&g_tmLQCD_lat);
+  if(exitstatus != 0) {
+    EXIT(16);
+  }
+#endif
+
+  /* initialize MPI parameters */
+  mpi_init(argc, argv);
+
+  /* initialize T etc. */
+  fprintf(stdout, "# [%2d] parameters:\n"\
+                  "# [%2d] T_global     = %3d\n"\
+                  "# [%2d] T            = %3d\n"\
+		  "# [%2d] Tstart       = %3d\n"\
+                  "# [%2d] LX_global    = %3d\n"\
+                  "# [%2d] LX           = %3d\n"\
+		  "# [%2d] LXstart      = %3d\n"\
+                  "# [%2d] LY_global    = %3d\n"\
+                  "# [%2d] LY           = %3d\n"\
+		  "# [%2d] LYstart      = %3d\n",\
+		  g_cart_id, g_cart_id, T_global, g_cart_id, T, g_cart_id, Tstart,
+		             g_cart_id, LX_global, g_cart_id, LX, g_cart_id, LXstart,
+		             g_cart_id, LY_global, g_cart_id, LY, g_cart_id, LYstart);
+
+  if(init_geometry() != 0) {
+    fprintf(stderr, "[test_sort] Error from init_geometry\n");
+    EXIT(101);
+  }
+
+  geometry();
+
+  mpi_init_xchange_eo_spinor();
+
+  unsigned int const Vhalf = VOLUME / 2;
+  unsigned int const VOL3  = LX*LY*LZ;
+
+
+  g_seed = 10000 + g_cart_id;
+  rlxd_init(2, g_seed);
+
+  unsigned int const N = 20;
+  
+  double *value = init_1level_dtable ( N );
+
+  rangauss ( value, N );
+
+  double ** v = init_2level_dtable ( N , _GSI(VOLUME));
+
+  rangauss ( v[0], N*_GSI(VOLUME) );
+
+  exitstatus = sort_fields_by_value ( v, value, N , _GSI(VOLUME) );
+
+
+  fini_1level_dtable ( &value );
+  fini_2level_dtable ( &v );
+
+  /***********************************************
+   * free the allocated memory, finalize 
+   ***********************************************/
+  free_geometry();
+
+#ifdef HAVE_MPI
+  mpi_fini_xchange_eo_spinor();
+  mpi_fini_datatypes();
+#endif
+
+  if (g_cart_id == 0) {
+    g_the_time = time(NULL);
+    fprintf(stdout, "# [test_sort] %s# [test_sort] end fo run\n", ctime(&g_the_time));
+    fflush(stdout);
+    fprintf(stderr, "# [test_sort] %s# [test_sort] end fo run\n", ctime(&g_the_time));
+    fflush(stderr);
+  }
+
+
+#ifdef HAVE_MPI
+  MPI_Finalize();
+#endif
+
+  return(0);
+}
+
