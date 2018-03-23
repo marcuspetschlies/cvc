27ecd0e9640adbbf062ed3982a2e82c35bb3db20
Marcus <marcus.petschlies@web.de>
2018-03-23 12:42:31 +0100

    tested vdagw momentum projection

--- test_vdagw.cpp
+++ test_vdagw.cpp
@@ -57,7 +57,7 @@ void usage(void) {
 
 int main(int argc, char **argv) {
   
-  int c, status; 
+  int c;
   int filename_set = 0;
   char filename[200];
 
@@ -95,16 +95,16 @@ int main(int argc, char **argv) {
   /*********************************
    * initialize MPI parameters for cvc
    *********************************/
-  status = tmLQCD_invert_init(argc, argv, 1);
-  if(status != 0) {
+  exitstatus = tmLQCD_invert_init(argc, argv, 1);
+  if( exitstatus != 0) {
     EXIT(14);
   }
-  status = tmLQCD_get_mpi_params(&g_tmLQCD_mpi);
-  if(status != 0) {
+  exitstatus = tmLQCD_get_mpi_params(&g_tmLQCD_mpi);
+  if( exitstatus != 0) {
     EXIT(15);
   }
-  status = tmLQCD_get_lat_params(&g_tmLQCD_lat);
-  if(status != 0) {
+  exitstatus = tmLQCD_get_lat_params(&g_tmLQCD_lat);
+  if( exitstatus != 0) {
     EXIT(16);
   }
 #endif
@@ -121,6 +121,9 @@ int main(int argc, char **argv) {
 
   mpi_init_xchange_eo_spinor();
 
+
+  int const io_proc = get_io_proc ();
+
   int exitstatus;
   unsigned int const Vhalf = VOLUME / 2;
   unsigned int const VOL3half = LX*LY*LZ / 2;
@@ -128,13 +131,15 @@ int main(int argc, char **argv) {
   unsigned int const nv = 11;
   unsigned int const nw =  7;
 
-  double ** v = init_2level_dtable ( nv, _GSI( (VOLUME+RAND)/2 ) );
-  double ** w = init_2level_dtable ( nw, _GSI( (VOLUME+RAND)/2 ) );
-
   double *** contr = init_3level_dtable ( nv, nw, 2*VOL3half );
 
   g_seed = 10000 + g_cart_id;
   rlxd_init(2, g_seed);
+
+#if 0
+
+  double ** v = init_2level_dtable ( nv, _GSI( (VOLUME+RAND)/2 ) );
+  double ** w = init_2level_dtable ( nw, _GSI( (VOLUME+RAND)/2 ) );
 
   /* set the spinor field */
   for ( unsigned int i = 0; i < nv; i++ ) {
@@ -150,6 +155,7 @@ int main(int argc, char **argv) {
     xchange_eo_field( w[i], 0 );
 #endif
   }
+
 
   for ( int t = 0; t <= T; t++ ) {
     exitstatus = vdag_w_spin_color_reduction ( contr, v, w, nv, nw, t );
@@ -192,13 +198,102 @@ int main(int argc, char **argv) {
 
     }}
   }
+  fini_2level_dtable ( &v );
+  fini_2level_dtable ( &w );
+
+#endif  // of if 0
+
+  rangauss ( contr[0][0], nv*nw* 2 * VOL3half );
+  for ( int mu = 0; mu < 4; mu++ ) {
+    int s[3] = {0,0,0};
+    if ( mu > 0 ) s[mu-1] = -1;
+
+    for ( int ieo = 0; ieo < 2; ieo++ ) {
+      if ( g_cart_id == 0 ) fprintf ( stdout, "# [test_vdagw] mu %d ieo %2d\n", mu, ieo );
+#ifdef HAVE_MPI
+      MPI_Barrier ( g_cart_grid );
+#endif
+      
+      for ( int t = 0; t < T; t++ ) {
+  
+        double _Complex *** contr_p = init_3level_ztable ( g_sink_momentum_number, nv, nw );
+        double _Complex *** contr_p2 = init_3level_ztable ( g_sink_momentum_number, nv, nw );
+
+        exitstatus = vdag_w_momentum_projection ( contr_p, contr, nv, nw, g_sink_momentum_list, g_sink_momentum_number, t, ieo, mu );
+
+        for ( int imom = 0; imom < g_sink_momentum_number; imom++ ) {
+
+          double const p[3] = {
+            2.*M_PI * g_sink_momentum_list[imom][0] / (double)LX_global,
+            2.*M_PI * g_sink_momentum_list[imom][1] / (double)LY_global,
+            2.*M_PI * g_sink_momentum_list[imom][2] / (double)LZ_global
+          };
+          double const phase_offset = g_proc_coords[1]*LX * p[0] + g_proc_coords[2]*LY * p[1] + g_proc_coords[3]*LZ * p[2];
+
+          unsigned int ixeo = 0;
+          for ( int x1 = 0; x1 < LX; x1++ ) {
+          for ( int x2 = 0; x2 < LY; x2++ ) {
+          for ( int x3 = 0; x3 < LZ; x3++ ) {
+
+            int const r[3] =  { x1, x2, x3 };
+
+            unsigned int const ix = g_ipt[t][x1][x2][x3];
+            if ( !g_iseven[ix] != ieo ) continue;
+           
+            double const phase = phase_offset + ( r[0] + s[0] ) * p[0] + ( r[1] + s[1] ) * p[1] + ( r[2] + s[2] ) * p[2];
+            double _Complex const ephase = cexp ( I*phase );
+
+            for ( unsigned int iv = 0; iv < nv; iv++ ) {
+            for ( unsigned int iw = 0; iw < nw; iw++ ) {
+              contr_p2[imom][iv][iw] += ( contr[iv][iw][2*ixeo] + I * contr[iv][iw][2*ixeo+1] ) * ephase;
+            }}
+
+            ixeo++;
+          }}}
+        }  // end of loop on momenta
+#ifdef HAVE_MPI
+#  if ( defined PARALLELTX ) || ( defined PARALLELTXY ) || ( defined PARALLELTXYZ )
+        void *buffer = malloc ( nv*nw*g_sink_momentum_number * 2 *sizeof (double) );
+        memcpy ( buffer,  contr_p2[0][0], nv*nw*g_sink_momentum_number * 2 * sizeof(double) );
+        MPI_Allreduce( buffer, contr_p2[0][0], 2*nv*nw*g_sink_momentum_number, MPI_DOUBLE, MPI_SUM, g_ts_comm );
+        free ( buffer );
+#  endif
+#endif
+
+        for ( int imom = 0; imom < g_sink_momentum_number; imom++ ) {
+          double const eps = 1.e-14;
+          for ( unsigned int iv = 0; iv < nv; iv++ ) {
+            for ( unsigned int iw = 0; iw < nw; iw++ ) {
+              double norm = 
+                cabs ( contr_p[imom][iv][iw] - contr_p2[imom][iv][iw] ) / ( 0.5 * cabs ( contr_p[imom][iv][iw] + contr_p2[imom][iv][iw] ) );
+              if ( io_proc >= 1 ) { 
+                fprintf ( stdout, " t %3d p %3d %3d %3d v %2d w %2d  c %25.16e %25.16e c2 %25.16e %25.16e  diff %25.16e\n", t + g_proc_coords[0]*T, 
+                    g_sink_momentum_list[imom][0], g_sink_momentum_list[imom][1], g_sink_momentum_list[imom][2],
+                    iv, iw, 
+                    creal ( contr_p[imom][iv][iw]  ), cimag ( contr_p[imom][iv][iw]  ), creal ( contr_p2[imom][iv][iw]  ), cimag ( contr_p2[imom][iv][iw]  ),
+                    norm );
+                if ( norm > eps ) 
+                  fprintf ( stderr, "[test_vdagw] Error for t %3d p %3d %3d %3d v %2d w %2d diff %25.16e\n", t + g_proc_coords[0]*T, 
+                      g_sink_momentum_list[imom][0], g_sink_momentum_list[imom][1], g_sink_momentum_list[imom][2],
+                      iv, iw, norm );
+                  
+              }
+            }
+          }
+        }
+
+        fini_3level_ztable ( &contr_p );
+        fini_3level_ztable ( &contr_p2 );
+      }  // end of loop on ieo
+
+    }  // end of loop on mu
+
+  }  // end of loop on timeslices
+    
 
   /***********************************************
    * free the allocated memory, finalize 
    ***********************************************/
-
-  fini_2level_dtable ( &v );
-  fini_2level_dtable ( &w );
 
   fini_3level_dtable ( &contr );
 
