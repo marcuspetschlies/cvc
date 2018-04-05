121c0b8d179701a3ca97472e9a550009f6be1ccf
Marcus <marcus.petschlies@web.de>
2018-04-05 17:08:53 +0200

    added prototype for twopoint_function_get_correlator_phase; const args in prototype twopoint_function_correlator_phase

--- twopoint_function_utils.h
+++ twopoint_function_utils.h
@@ -8,7 +8,9 @@ void twopoint_function_init ( twopoint_function_type *p );
 void twopoint_function_print ( twopoint_function_type *p, char *name, FILE*ofs );
 void twopoint_function_print_diagram_key (char*key, twopoint_function_type *p, int id );
 void twopoint_function_print_correlator_key (char*key, twopoint_function_type *p );
-void twopoint_function_correlator_phase (double _Complex *c, twopoint_function_type *p, unsigned int N);
+
+void twopoint_function_correlator_phase (double _Complex * const c, twopoint_function_type *p, unsigned int const N);
+
 void twopoint_function_print_correlator_data ( double _Complex *c, twopoint_function_type *p, FILE*ofs );
 
 void twopoint_function_get_aff_filename_prefix (char*filename, twopoint_function_type*p);
@@ -16,6 +18,8 @@ void twopoint_function_get_aff_filename_prefix (char*filename, twopoint_function
 void twopoint_function_copy ( twopoint_function_type *p, twopoint_function_type *r );
 
 void twopoint_function_get_diagram_norm ( double*r, twopoint_function_type *p, int id );
+
+double _Complex twopoint_function_get_correlator_phase ( twopoint_function_type *p );
 
 }  /* end of namespace cvc */
 
