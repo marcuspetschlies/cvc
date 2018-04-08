3ba47e347b4176c0f870e258c15bad58ba35bd74
Marcus Petschlies <marcus.petschlies@web.de>
2018-04-08 22:23:37 +0200

    changed to use of table_init

--- rotations.h
+++ rotations.h
@@ -2,6 +2,7 @@
 #define _ROTATIONS_H
 
 #include "ilinalg.h"
+#include "table_init_z.h"
 
 namespace cvc {
 
@@ -207,17 +208,16 @@ inline void rot_reduce_point_bnd ( int nred[3], int n[3] ) {
  ***********************************************************/
 inline double _Complex **rot_init_rotation_matrix (int N ) {
 
-  double _Complex **SSpin = NULL;
-  int exitstatus = init_2level_buffer( (double***)&SSpin, N, 2*N );
-  if ( exitstatus != 0 ) {
-    fprintf(stderr, "[rot_bispinor_rotation_matrix_spherical_basis] Error from init_2level_buffer, status was %d\n", exitstatus);
+  double _Complex ** SSpin = init_2level_ztable ( N, N );
+  if ( SSpin == NULL ) {
+    fprintf(stderr, "[rot_init_rotation_matrix] Error from init_2level_ztable %s %d\n", __FILE__, __LINE__);
     return(NULL);
   }
   return(SSpin);
 }  /* end of rot_init_rotation_matrix */
 
 inline double _Complex **rot_fini_rotation_matrix ( double _Complex ***R ) {
-  fini_2level_buffer( (double***)R );
+  fini_2level_ztable ( R );
   return(NULL);
 }  /* end of rot_fini_rotation_matrix */
 
