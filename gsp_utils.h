92ab44e9a057e9a39385e01f962496df7f09b69f
Marcus Petschlies <marcus.petschlies@web.de>
2018-04-08 22:23:07 +0200

    adapted prototype of gsp_write_eval

--- gsp_utils.h
+++ gsp_utils.h
@@ -7,7 +7,7 @@ namespace cvc {
 
 int gsp_read_node (double ***gsp, int num, int momentum[3], int gamma_id, char*tag, int timeslice);
 
-int gsp_write_eval(double *eval, int num, char*tag);
+int gsp_write_eval(double * const eval, unsigned int const num, char * const filename_prefix, char * const tag);
 
 int gsp_read_eval(double **eval, int num, char*tag);
 
