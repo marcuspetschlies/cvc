4255e7743575d30b4aa97dae878b1e486612ba2d
Marcus <marcus.petschlies@web.de>
2018-04-09 17:56:24 +0200

    adapted prototype of aff_key_conversion_diagram to const arguments, except for key

--- aff_key_conversion.h
+++ aff_key_conversion.h
@@ -9,7 +9,8 @@ int v2_key_index_conversion ( double _Complex *buffer, int perm[4], int N, int L
 
 int vn_oet_read_key ( double _Complex *key_buffer, char*tag, int i_sample, int pi2[3], int pf1[3], int pf2[3], int source_coords[4], int gamma_id, struct AffReader_s *affr );
 
-void aff_key_conversion_diagram (  char*key, char*tag, int pi1[3], int pi2[3], int pf1[3], int pf2[3], int gi1, int gi2, int gf1, int gf2, int source_coords[4], char*diag_name, int diag_id );
+void aff_key_conversion_diagram (  char*key, char * const tag, int const pi1[3], int const pi2[3], int const pf1[3], int const pf2[3], 
+        int const gi1, int const gi2, int const gf1, int const gf2, int const source_coords[4], char * const diag_name, int const diag_id );
 
 void gamma_name_to_gamma_signed_id (int *id, double*sign, char *name );
 
