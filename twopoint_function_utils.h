8288444e4b875dbc498f4061733b9dea85f46b2e
Marcus <marcus.petschlies@web.de>
2018-04-10 19:16:59 +0200

    adapted prototypes

--- twopoint_function_utils.h
+++ twopoint_function_utils.h
@@ -11,15 +11,19 @@ void twopoint_function_print_correlator_key (char*key, twopoint_function_type *p
 
 void twopoint_function_correlator_phase (double _Complex * const c, twopoint_function_type *p, unsigned int const N);
 
-void twopoint_function_print_correlator_data ( double _Complex *c, twopoint_function_type *p, FILE*ofs );
-
+void twopoint_function_print_correlator_data ( double _Complex * const c, twopoint_function_type *p, FILE*ofs, int const N );
+  
 void twopoint_function_get_aff_filename_prefix (char*filename, twopoint_function_type*p);
 
 void twopoint_function_copy ( twopoint_function_type *p, twopoint_function_type *r );
 
-void twopoint_function_get_diagram_norm ( double*r, twopoint_function_type *p, int id );
+double twopoint_function_get_diagram_norm ( twopoint_function_type *p, int const id );
 
 double _Complex twopoint_function_get_correlator_phase ( twopoint_function_type *p );
+
+int twopoint_function_accumulate_diagrams ( double _Complex *** const diagram, twopoint_function_type *p, int const N, struct AffReader_s *affr );
+
+
 
 }  /* end of namespace cvc */
 
