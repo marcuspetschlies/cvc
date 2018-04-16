7656237cf1bb196da0ed43a814ae8b980ee5ad04
Marcus <marcus.petschlies@web.de>
2018-04-16 17:04:04 +0200

    corrections in init_2level_ztable_asym, adaptions to the former in the other init functions

--- table_init_asym_z.h
+++ table_init_asym_z.h
@@ -20,10 +20,11 @@ namespace cvc {
 /************************************************************************************
  * (de-)allocate 2-level asymmetric buffer (n1 x dim[i] double _Complex matrix)
  ************************************************************************************/
-inline double _Complex ** init_2level_ztable_asym ( int const N0, int * const dim) {
+inline double _Complex ** init_2level_ztable_asym ( int const N0, int * const dim, int const ndim ) {
+
 
   // 1st, outer level
-  double _Complex ** s = (double _Complex**)malloc( N0 * sizeof(double _Complex*));
+  double _Complex ** s = (double _Complex**)malloc( N0 * ndim * sizeof(double _Complex*));
   if( s == NULL ) {
     fprintf(stderr, "[init_2level_zbuffer_asym] Error from malloc %s %d\n", __FILE__, __LINE__); 
     return( NULL );
@@ -31,15 +32,24 @@ inline double _Complex ** init_2level_ztable_asym ( int const N0, int * const di
 
   // 2nd, inner level
   size_t items = 0;
-  for ( int i = 0; i < N0; i++) items += (size_t)dim[i];
+  for ( int i = 0; i < ndim; i++) items += (size_t)dim[i];
+  size_t const bytes = N0 * items * sizeof(double _Complex);
 
-  s[0] = (double _Complex*)malloc( items * sizeof(double _Complex) );
+  s[0] = (double _Complex*)malloc( bytes );
   if( s[0] == NULL ) {
-    fprintf(stderr, "[init_2level_zbuffer_asym] Error from malloc %s %d\n", __FILE__, __LINE__); 
+    fprintf(stderr, "[init_2level_zbuffer_asym] Error from malloc for items = %lu %s %d\n", items, __FILE__, __LINE__); 
     return( NULL );
   }
-  for( int i = 1; i < N0; i++) s[i] = s[i-1] + dim[i-1];
-  memset( s[0], 0, items * sizeof(double _Complex) );
+
+  for( int i = 0; i < N0; i++) {
+    double _Complex ** const s_ = s + i * ndim;
+    s_[0] = s[0] + i * items;
+
+    for ( int k = 1; k < ndim; k++ ) {
+      s_[k] = s_[k-1] + dim[k-1];
+    }
+  }
+  memset( s[0], 0, bytes );
 
   return( s );
 }  // end of init_2level_ztable_asym
@@ -50,7 +60,7 @@ inline double _Complex ** init_2level_ztable_asym ( int const N0, int * const di
 
 inline double _Complex *** init_3level_ztable_asym (int const N0, int const N1, int * const dim ) {
 
-  double _Complex ** s__ = init_2level_ztable_asym ( N0*N1, dim );
+  double _Complex ** s__ = init_2level_ztable_asym ( N0, dim, N1 );
   if ( s__ == NULL ) return( NULL );
 
   double _Complex *** s_ = ( double _Complex ***) malloc( N0 * sizeof( double _Complex **) );
@@ -91,7 +101,7 @@ inline void fini_2level_ztable_asym ( double _Complex *** s ) {
 
 inline void fini_3level_ztable_asym ( double _Complex **** s  ) {
   if ( *s != NULL ) {
-    fini_2level_ztable ( *s );
+    fini_2level_ztable_asym ( *s );
     free ( *s );
     *s = NULL;
   }
@@ -102,7 +112,7 @@ inline void fini_3level_ztable_asym ( double _Complex **** s  ) {
 
 inline void fini_4level_ztable_asym ( double _Complex ***** s  ) {
   if ( *s != NULL ) {
-    fini_3level_ztable ( *s );
+    fini_3level_ztable_asym ( *s );
     free ( *s );
     *s = NULL;
   }
