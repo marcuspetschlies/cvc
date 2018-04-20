7d805c6d593ed3d329eacd5554b39d646f4cbc99
Marcus <marcus.petschlies@web.de>
2018-04-20 17:11:07 +0200

    some new functionality

--- group_projection_applicator.h
+++ group_projection_applicator.h
@@ -27,6 +27,10 @@
 
 namespace cvc {
 
+inline int compare_momentum_configuration ( int ** const p, int ** const q, int const n );
+inline int compare_spinor_configuration ( double _Complex ** const v, double _Complex ** const w, int const n, int * const dim );
+inline int compare_spinor_pm_configuration ( double _Complex ** const v, double _Complex ** const w, int const n, int * const dim );
+
 double const eps = 9.e-15;
 
 typedef struct {
@@ -38,6 +42,7 @@ typedef struct {
   double _Complex ****v;
   double _Complex **c;
   char name[200];
+  char rotation_name[2][48][20];
 } little_group_projector_applicator_type;
 
 /***********************************************************
@@ -137,10 +142,139 @@ inline little_group_projector_applicator_type * copy_little_group_projector_appl
   return( a );
 }  // end of copy_little_group_projector_applicator
 
+
 /***********************************************************/
 /***********************************************************/
 
 /***********************************************************
+ *
+ ***********************************************************/
+inline little_group_projector_applicator_type * reduce_little_group_projector_applicator ( little_group_projector_applicator_type * const a, int const keep_direction ) {
+
+  /***********************************************************
+   * set coefficients to zero if they are below eps in absolute
+   * value
+   ***********************************************************/
+  for ( int i = 0; i < 2*a->rotation_n; i++ ) {
+    if ( cabs ( a->c[0][i] ) < eps ) a->c[0][i] = 0.;
+  }
+
+#if 0
+  // loop on parity parts
+  for ( int iparity = 0; iparity < 2; iparity++ ) {
+
+    // loop on rotations
+    for ( int i = 0; i < a->rotation_n-1; i++ ) {
+      if ( a->c[iparity][i] == 0 ) continue;
+      for ( int k = i+1; k < a->rotation_n; k++ ) {
+        if ( a->c[iparity][k] == 0. ) continue;
+        int const mom_config_is_equal = compare_momentum_configuration ( a->prot[iparity][i], a->prot[iparity][k], a->interpolator_n );
+        if ( mom_config_is_equal == 0 ) continue;
+
+
+        int const spinor_config_is_equal = keep_direction ? \
+                                           compare_spinor_configuration ( a->v[iparity][i], a->v[iparity][k], a->interpolator_n, a->interpolator_dim ) : \
+                                           compare_spinor_pm_configuration ( a->v[iparity][i], a->v[iparity][k], a->interpolator_n, a->interpolator_dim );
+
+        if ( spinor_config_is_equal ) {
+          a->c[iparity][i] += a->c[iparity][k] * spinor_config_is_equal;
+          a->c[iparity][k] = 0.;
+        }
+      }  // end of loop on to-compare-to rotations
+    }  // end of loop on rotations
+  }  // end loop on parity parts
+#endif
+
+  // loop on rotations
+  for ( int i = 0; i < 2*a->rotation_n-1; i++ ) {
+    if ( a->c[0][i] == 0 ) continue;
+      for ( int k = i+1; k < 2*a->rotation_n; k++ ) {
+        if ( a->c[0][k] == 0. ) continue;
+        int const mom_config_is_equal = compare_momentum_configuration ( a->prot[0][i], a->prot[0][k], a->interpolator_n );
+        if ( mom_config_is_equal == 0 ) continue;
+
+
+        int const spinor_config_is_equal = keep_direction ? \
+                                           compare_spinor_configuration ( a->v[0][i], a->v[0][k], a->interpolator_n, a->interpolator_dim ) : \
+                                           compare_spinor_pm_configuration ( a->v[0][i], a->v[0][k], a->interpolator_n, a->interpolator_dim );
+
+        if ( spinor_config_is_equal ) {
+          a->c[0][i] += a->c[0][k] * spinor_config_is_equal;
+          a->c[0][k] = 0.;
+        }
+      }  // end of loop on to-compare-to rotations
+    }  // end of loop on rotations
+
+  return( a );
+}  // end of reduce_little_group_projector_applicator
+
+/***********************************************************/
+/***********************************************************/
+
+inline int compare_momentum_configuration ( int ** const p, int ** const q, int const n ) {
+  int isequal = 1;
+
+  for ( int i = 0; i < n; i++ ) {
+    isequal &= ( p[i][0] == q[i][0] ) && ( p[i][1] == q[i][1] ) && ( p[i][2] == q[i][2] );
+  }
+  return( isequal );
+}
+
+/***********************************************************/
+/***********************************************************/
+
+/***********************************************************
+ * returns
+ * 1    if v[i] == w[i] for all spinors i
+ * +/-2 if v[i] == +/- w[i] for all spinors i;
+ *   sign is the product of all sign changes
+ ***********************************************************/
+inline int compare_spinor_configuration ( double _Complex ** const v, double _Complex ** const w, int const n, int * const dim ) {
+  int isequal = 1;
+
+  for ( int i = 0; i < n; i++ ) {
+    int iep = 1;
+    for ( int k = 0; k < dim[i]; k++ ) {
+      iep &= cabs ( v[i][k] - w[i][k] ) < eps;
+    }  // end of loop on components
+    isequal &= iep;
+  }  // end of loop on spinors
+
+  return( isequal );
+}  // end of compare_spinor_configuration
+
+/***********************************************************/
+/***********************************************************/
+
+/***********************************************************
+ * returns
+ * +/-1 if v[i] == +/- w[i] for all spinors i;
+ *   sign is the product of all sign changes
+ ***********************************************************/
+inline int compare_spinor_pm_configuration ( double _Complex ** const v, double _Complex ** const w, int const n, int * const dim ) {
+  int isequalpm = 1;
+  int sign = 1;
+
+  for ( int i = 0; i < n; i++ ) {
+    int iep = 1;
+    int iem = 1;
+    for ( int k = 0; k < dim[i]; k++ ) {
+      iep &= cabs ( v[i][k] - w[i][k] ) < eps;
+      iem &= cabs ( v[i][k] + w[i][k] ) < eps;
+    }  // end of loop on components
+    if ( iem ) sign = -sign;
+    isequalpm &= iem || iep;
+  }  // end of loop on spinors
+
+  return ( sign*isequalpm );
+}  // end of compare_spinor_pm_configuration
+
+
+/***********************************************************/
+/***********************************************************/
+
+/***********************************************************
+ *
  ***********************************************************/
 inline little_group_projector_applicator_type * show_little_group_projector_applicator ( little_group_projector_applicator_type * a, FILE*ofs ) {
 
@@ -155,6 +289,13 @@ inline little_group_projector_applicator_type * show_little_group_projector_appl
   fprintf ( myofs, "# [show_little_group_projector_applicator] rotations     %3d\n", a->rotation_n );
   fprintf ( myofs, "# [show_little_group_projector_applicator] interpolators %3d\n", a->interpolator_n );
 
+  /***********************************************************
+   * reduce the applicator
+   ***********************************************************/
+  reduce_little_group_projector_applicator ( a, 1 );
+
+  /***********************************************************/
+  /***********************************************************/
 
   /***********************************************************
    * now print the complete projector information
@@ -169,7 +310,7 @@ inline little_group_projector_applicator_type * show_little_group_projector_appl
 
       // show the overall coefficient
       if ( cabs ( a->c[iparity][irot] ) < eps ) continue;
-      fprintf ( myofs, "# [show_little_group_projector_applicator] rot %2d par %2d", irot+1 , 1 - 2*iparity);
+      fprintf ( myofs, "# [show_little_group_projector_applicator] rot %2d par %2d elem %-8s", irot+1 , 1 - 2*iparity, a->rotation_name[iparity][irot] );
       // fprintf ( myofs, "  c  %25.16e %25.16e\n", dgeps( creal( a->c[iparity][irot]), eps ), dgeps( cimag( a->c[iparity][irot]), eps ) );
 
       for ( int iop = 0; iop < a->interpolator_n; iop++ ) {
@@ -208,70 +349,5 @@ inline little_group_projector_applicator_type * show_little_group_projector_appl
 
 /***********************************************************/
 /***********************************************************/
-
-/***********************************************************
- *
- ***********************************************************/
-inline little_group_projector_applicator_type * reduce_little_group_projector_applicator ( little_group_projector_applicator_type * const a ) {
-
-  /***********************************************************
-   * set coefficients to zero if they are below eps in absolute
-   * value
-   ***********************************************************/
-  for ( int i = 0; i < 2*a->rotation_n; i++ ) {
-    if ( cabs ( a->c[0][i] ) < eps ) a->c[0][i] = 0.;
-  }
-
-
-typedef struct {
-    int rotation_n;
-      int interpolator_n;
-        int *interpolator_dim;
-          int P[3];
-            int **** prot;
-              double _Complex ****v;
-                double _Complex **c;
-                  char name[200];
-} little_group_projector_applicator_type;
-
-  for ( int iparity = 0; iparity < 2; iparity++ ) {
-    for ( int i = 0; i < a->rotation_n-1; i++ ) {
-      if ( a->c[iparity][i] == 0 ) continue;
-      for ( int k = i+1; k < a->rotation_n; k++ ) {
-        if ( ( compare_momentum_configuration ( a->prot[iparity][i], a->prot[iparity][k], a->interpolator_n ) == 1 ) && a->c[iparity][k] != 0. ) {
-          a->c[iparity][i] += a->c[iparity][k];
-          a->c[iparity][k] = 0.;
-        }
-      }
-    }
-  }
-  
-
-}  // end of reduce_little_group_projector_applicator
-
-/***********************************************************/
-/***********************************************************/
-
-inline int compare_momentum_configuration ( int ** const p, int ** const q, int const n ) {
-  int isequal = 1;
-
-  for ( int i = 0; i < n; i++ ) {
-    isequal &= ( p[i][0] == q[i][0] ) && ( p[i][1] == q[i][1] ) && ( p[i][2] == q[i][2] );
-  }
-  return( isequal );
-}
-
-inline int compare_zvector_configuration ( double _Complex ** const v, double _Complex ** const w, int const n, int * const dim ) {
-  int isequalp = 1;
-  int isequaln = 1;
-
-  for ( int i = 0; i < n; i++ ) {
-    for ( int k = 0; k < dim[i]; k++ ) {
-      isequalp &= cabs ( v[i][k] - w[i][k] ) < 
-    }
-  }
-
-  return( isequal );
-}  // end of compare_zvector_configuration
 
 }  // end of namespace cvc
