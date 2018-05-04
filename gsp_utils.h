7e8562c5f2e2f93ca53c14b1237d3fe2f7974f27
Marcus <marcus.petschlies@web.de>
2018-05-04 17:01:49 +0200

    added prototype for gsp_read_cvc_node

--- gsp_utils.h
+++ gsp_utils.h
@@ -11,6 +11,19 @@ int gsp_write_eval(double * const eval, unsigned int const num, char * const fil
 
 int gsp_read_eval(double ** eval, unsigned int num, char * const filename_prefix, char * const tag);
 
+int gsp_read_cvc_node (
+  double _Complex ** const fac,
+  unsigned int const numV,
+  unsigned int const block_length,
+  int const momentum[3],
+  char * const type,
+  int const mu,
+  char * const prefix,
+  char * const tag,
+  int const timeslice,
+  unsigned int const nt
+);
+
 /***************************************************/
 /***************************************************/
 
