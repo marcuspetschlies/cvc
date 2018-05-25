4dbff3087a54c1f253366bf6f67cf064a0ff794a
Marcus <marcus.petschlies@web.de>
2018-05-25 16:50:24 +0200

    reworked

--- contract_cvc_tensor_eo_lm_mee.cpp
+++ contract_cvc_tensor_eo_lm_mee.cpp
@@ -49,21 +49,27 @@ int const contract_cvc_tensor_eo_lm_mee (
   double * const w = eo_spinor_field[ 1];
   
   // Gamma_mu^F v
-  double * const gmufv[4] = { eo_spinor_field[ 2], eo_spinor_field[ 3], eo_spinor_field[ 4], eo_spinor_field[ 5] };
+  double * const gfv[4] = { eo_spinor_field[ 2], eo_spinor_field[ 3], eo_spinor_field[ 4], eo_spinor_field[ 5] };
   // Gamma_mu^B v
-  double * const gmubv[4] = { eo_spinor_field[ 6], eo_spinor_field[ 7], eo_spinor_field[ 8], eo_spinor_field[ 9] };
+  double * const gbv[4] = { eo_spinor_field[ 6], eo_spinor_field[ 7], eo_spinor_field[ 8], eo_spinor_field[ 9] };
   // Gamma_mu^F w
-  double * const gmufw[4] = { eo_spinor_field[10], eo_spinor_field[11], eo_spinor_field[12], eo_spinor_field[13] };
+  double * const gfw[4] = { eo_spinor_field[10], eo_spinor_field[11], eo_spinor_field[12], eo_spinor_field[13] };
   // Gamma_mu^B w
-  double * const gmubw[4] = { eo_spinor_field[14], eo_spinor_field[15], eo_spinor_field[16], eo_spinor_field[17] };
+  double * const gbw[4] = { eo_spinor_field[14], eo_spinor_field[15], eo_spinor_field[16], eo_spinor_field[17] };
 
   /***********************************************************
    * gather scalar products for all eigenvectors
    ***********************************************************/
   double _Complex ***** meesp = NULL;
+  
+  /***********************************************************
+   * NOTE: this is a large array for typical lattice size
+   * and O(10^3) low modes, but ONLY 1 allocation per
+   * timeslice
+   ***********************************************************/
   if (g_ts_id == 0 ) {
     //                           mu x nu x timeslice x eigenvector x 4 scalar products
-    meesp = init_5level_ztable ( 4,   4,   T,          nev,          4 );
+    meesp = init_5level_ztable ( 4,   4,   T_global,   nev,          4 );
     if ( meesp == NULL )  {
       fprintf(stderr, "# [contract_cvc_tensor_eo_lm_mee] Error from init_5level_ztable %s %d\n", __FILE__, __LINE__);
       return(2);
@@ -89,22 +95,24 @@ int const contract_cvc_tensor_eo_lm_mee (
 
       memcpy ( eo_spinor_work[0], v, sizeof_eo_spinor_field );
       /* Gmufwdr V */
-      apply_cvc_vertex_eo( gmufv[imu], eo_spinor_work[0], imu, 0, gauge_field, 0 );
+      apply_cvc_vertex_eo( gfv[imu], eo_spinor_work[0], imu, 0, gauge_field, 0 );
       /* Gmubwdr V */
-      apply_cvc_vertex_eo( gmubv[imu], eo_spinor_work[0], imu, 1, gauge_field, 0 );
+      apply_cvc_vertex_eo( gbv[imu], eo_spinor_work[0], imu, 1, gauge_field, 0 );
 
       memcpy ( eo_spinor_work[0], w, sizeof_eo_spinor_field );
       /* Gmufwdr W */
-      apply_cvc_vertex_eo( gmufw[imu], eo_spinor_work[0], imu, 0, gauge_field, 0 );
+      apply_cvc_vertex_eo( gfw[imu], eo_spinor_work[0], imu, 0, gauge_field, 0 );
       /* Gmubwdr W */
-      apply_cvc_vertex_eo( gmubw[imu], eo_spinor_work[0], imu, 1, gauge_field, 0 );
+      apply_cvc_vertex_eo( gbw[imu], eo_spinor_work[0], imu, 1, gauge_field, 0 );
     }
 
+    // multiply gfv and gbv fields by g5
     g5_phi ( eo_spinor_field[2], 8 * Vhalf );
 
+    // multiply gfw and gbw fields by M_ee^-1
     for ( int imu = 0; imu < 4; imu++ ) {
-      M_clover_zz_inv_matrix ( gmufw[imu], gmufw[imu], mzzinv[0][0] );
-      M_clover_zz_inv_matrix ( gmubw[imu], gmubw[imu], mzzinv[0][0] );
+      M_clover_zz_inv_matrix ( gfw[imu], gfw[imu], mzzinv[0][0] );
+      M_clover_zz_inv_matrix ( gbw[imu], gbw[imu], mzzinv[0][0] );
     }
 
     double _Complex ** p = init_2level_ztable ( 2, T );
@@ -124,40 +132,60 @@ int const contract_cvc_tensor_eo_lm_mee (
 
         // the 8 scalar products
 
-        eo_spinor_spatial_scalar_product_co( p[0], gmubv[imu], gmufw[inu], 0 );
-        eo_spinor_spatial_scalar_product_co( p[1], gmufv[inu], gmubw[imu], 0 );
+        // (1)
+        eo_spinor_spatial_scalar_product_co( p[0], gbv[imu], gfw[inu], 0 );
+        // (2)
+        eo_spinor_spatial_scalar_product_co( p[1], gfv[inu], gbw[imu], 0 );
         if ( g_ts_id == 0 ) {
 #ifdef HAVE_OPENMP
 #pragma omp parallel for
 #endif
-          for ( int it = 0; it < T; it++ ) meesp[imu][inu][it][inev][0] = p[0][it] + p[1][it];
+          for ( int it = 0; it < T; it++ ) {
+            int const x0 = ( it + g_proc_coords[0]*T - ( imu == 0 ) + T_global ) % T_global;
+            meesp[imu][inu][x0][inev][0] = p[0][it] + p[1][it];
+          }
         }
 
-        eo_spinor_spatial_scalar_product_co( p[0], gmufv[imu], gmufw[inu], 0 );
-        eo_spinor_spatial_scalar_product_co( p[1], gmufv[inu], gmufw[imu], 0 );
+        // (3)
+        eo_spinor_spatial_scalar_product_co( p[0], gbv[inu], gfw[imu], 0 );
+        // (4)
+        eo_spinor_spatial_scalar_product_co( p[1], gfv[imu], gbw[inu], 0 );
         if ( g_ts_id == 0 ) {
 #ifdef HAVE_OPENMP
 #pragma omp parallel for
 #endif
-          for ( int it = 0; it < T; it++ ) meesp[imu][inu][it][inev][1] = p[0][it] + p[1][it];
+          for ( int it = 0; it < T; it++ ) {
+            int const x0 = it + g_proc_coords[0] * T;
+            meesp[imu][inu][x0][inev][1] = p[0][it] + p[1][it];
+          }
         }
 
-        eo_spinor_spatial_scalar_product_co( p[0], gmubv[inu], gmufw[imu], 0 );
-        eo_spinor_spatial_scalar_product_co( p[1], gmufv[imu], gmubw[inu], 0 );
+        // (5)
+        eo_spinor_spatial_scalar_product_co( p[0], gfv[imu], gfw[inu], 0 );
+        // (6)
+        eo_spinor_spatial_scalar_product_co( p[1], gfv[inu], gfw[imu], 0 );
         if ( g_ts_id == 0 ) {
 #ifdef HAVE_OPENMP
 #pragma omp parallel for
 #endif
-          for ( int it = 0; it < T; it++ ) meesp[imu][inu][it][inev][2] = p[0][it] + p[1][it];
+          for ( int it = 0; it < T; it++ ) {
+            int const x0 = it + g_proc_coords[0] * T;
+            meesp[imu][inu][x0][inev][2] = p[0][it] + p[1][it];
+          }
         }
 
-        eo_spinor_spatial_scalar_product_co( p[0], gmubv[inu], gmubw[imu], 0 );
-        eo_spinor_spatial_scalar_product_co( p[1], gmubv[imu], gmubw[inu], 0 );
+        // (7)
+        eo_spinor_spatial_scalar_product_co( p[0], gbv[inu], gbw[imu], 0 );
+        // (8)
+        eo_spinor_spatial_scalar_product_co( p[1], gbv[imu], gbw[inu], 0 );
         if ( g_ts_id == 0 ) {
 #ifdef HAVE_OPENMP
 #pragma omp parallel for
 #endif
-          for ( int it = 0; it < T; it++ ) meesp[imu][inu][it][inev][3] = p[0][it] + p[1][it];
+          for ( int it = 0; it < T; it++ ) {
+            int const x0 = ( it + g_proc_coords[0]*T - ( imu == 0 ) + T_global ) % T_global;
+            meesp[imu][inu][x0][inev][3] = p[0][it] + p[1][it];
+          }
         }
       }
     }
@@ -188,31 +216,36 @@ int const contract_cvc_tensor_eo_lm_mee (
 
 #ifdef HAVE_MPI
 #  if ( defined PARALLELTX ) || ( defined PARALLELTXY ) || ( defined PARALLELTXYZ ) 
-      // gather at g_tr_id = 0 and g_ts_id == 0
       // fprintf ( stdout, "# [contract_cvc_tensor_eo_lm_mee] proc %d does send/recv %s %d\n", g_cart_id, __FILE__, __LINE__ );
-      exitstatus = MPI_Gather( meesp[imu][inu][0][0], 2*T*nev*4, MPI_DOUBLE, meesp_buffer[0][0], 2*T*nev*4, MPI_DOUBLE, 0, g_tr_comm );
-      // exitstatus = MPI_Allgather( meesp[imu][inu][0][0], 2*T*nev*4, MPI_DOUBLE, meesp_buffer[0][0], 2*T*nev*4, MPI_DOUBLE, g_tr_comm );
-
+      
+      exitstatus = MPI_Allreduce ( meesp[imu][inu][0][0], meesp_buffer[0][0], 2 * T_global * nev * 4, MPI_DOUBLE, MPI_SUM,  g_tr_comm );
 #  else
-      exitstatus = MPI_Gather( meesp[imu][inu][0][0], 2*T*nev*4, MPI_DOUBLE, meesp_buffer[0][0], 2*T*nev*4, MPI_DOUBLE, 0, g_cart_grid );
+      exitstatus = MPI_Allreduce ( meesp[imu][inu][0][0], meesp_buffer[0][0], 2 * T_global * nev * 4, MPI_DOUBLE, MPI_SUM,  g_cart_grid );
 #  endif
       if ( exitstatus != MPI_SUCCESS ) {
-        fprintf ( stderr, "[contract_cvc_tensor_eo_lm_mee] Error from MPI_Gather %s %d\n", __FILE__, __LINE__ );
+        fprintf ( stderr, "[contract_cvc_tensor_eo_lm_mee] Error from MPI_Allreduce %s %d\n", __FILE__, __LINE__ );
         return(7);
-      } else {
-        fprintf ( stdout, "# [contract_cvc_tensor_eo_lm_mee] MPI_Gather was successful %s %d\n", __FILE__, __LINE__ );
       }
 #else
       memcpy ( meesp_buffer[0][0], meesp[imu][inu][0][0], 4 * T_global * nev * sizeof(double _Complex) );
 #endif
 
-      // process with time ray id 0 and time slice id 0
+      /***********************************************************
+       * process with time ray id 0 and time slice id 0
+       ***********************************************************/
       if ( g_tr_id == 0 ) {
 
-        int const dt[4] = { (imu == 0), 0, -(inu == 0), (imu == 0) - (inu == 0 ) };
+        int const dt[4] = { 
+            -( imu == 0 ),
+            +( inu == 0 ),
+            0, 
+            -(imu == 0) + (inu == 0 ) 
+        };
         if ( g_verbose > 3 ) fprintf ( stdout, "# [contract_cvc_tensor_eo_lm_mee] dt values %3d %3d %3d %3d\n", dt[0], dt[1], dt[2], dt[3] );
 
-        // loop on momenta
+        /***********************************************************
+         * loop on momenta
+         ***********************************************************/
         for ( unsigned int imom = 0; imom < momentum_number; imom++ ) {
 
           double const mom[4] = {
@@ -221,10 +254,10 @@ int const contract_cvc_tensor_eo_lm_mee (
             2. * M_PI * momentum_list[imom][1] / LY_global,
             2. * M_PI * momentum_list[imom][2] / LZ_global };
           double _Complex const phase[4] = {
-            cos ( mom[imu] ) + I * sin( mom[imu] ),
+            cexp ( -mom[imu] * I ),
+            cexp ( +mom[inu] * I ),
             1.,
-            cos ( mom[inu] ) - I * sin( mom[inu] ),
-            cos ( mom[imu] - mom[inu] ) + I * sin( mom[imu] - mom[inu] ) };
+            cexp ( ( -mom[imu] + mom[inu] ) * I ) };
 
           double _Complex *** meesp_out = init_3level_ztable ( T_global, 3, nev );
           if ( meesp_out == NULL ) {
@@ -232,14 +265,14 @@ int const contract_cvc_tensor_eo_lm_mee (
             return(8);
           }
 
-          // loop on timeslices
+          /***********************************************************
+           * loop on timeslices
+           ***********************************************************/
           for ( int it = 0; it < T_global; it++ ) {
  
-            int const iit = (it + ( inu==0) ) % T_global;
-            int const tval[4] = { it, it, iit, iit };
-            if ( g_verbose > 4 ) fprintf ( stdout, "# [contract_cvc_tensor_eo_lm_mee] t values %3d %3d %3d %3d\n", tval[0], tval[1], tval[2], tval[3] );
-        
-            // loop on eigenvectors
+            /***********************************************************
+             * loop on eigenvectors
+             ***********************************************************/
 #ifdef HAVE_OPENMP
 #pragma omp parallel for
 #endif
@@ -248,12 +281,14 @@ int const contract_cvc_tensor_eo_lm_mee (
               // combine the 4 scalar products with corresponding phase; sort according to dt
               for ( unsigned int isp = 0; isp < 4; isp++ )
               {
-                meesp_out[it][dt[isp]+1][iev] += meesp_buffer[tval[isp]][iev][isp] * phase[isp];
+                meesp_out[it][dt[isp]+1][iev] += meesp_buffer[it][iev][isp] * phase[isp];
               }
 
             }  // end of loop on eigenvectors
-             
-            // loop on dt
+
+            /***********************************************************
+             * loop on dt
+             ***********************************************************/
             for ( int idt = 0; idt < 3; idt++ ) {
               char aff_tag[200];
               sprintf ( aff_tag, "/%s/mu%d/nu%d/px%.2dpy%.2dpz%.2d/t%.2d/dt%d",
@@ -266,12 +301,9 @@ int const contract_cvc_tensor_eo_lm_mee (
                 fprintf ( stderr, "[contract_cvc_tensor_eo_lm_mee] Error from aff_node_put_complex, status was %d %s %d\n", exitstatus, __FILE__, __LINE__);
                 return(15);
               }
-          
             }  // end of loop on dt
-
           }  // end of loop on global timeslices
 
-        
           fini_3level_ztable ( &meesp_out );
         }  // end of loop on 3-momenta
 
@@ -283,7 +315,7 @@ int const contract_cvc_tensor_eo_lm_mee (
     }}  // end of loop on nu, mu
 
     // if ( g_tr_id == 0 ) {
-      fini_3level_ztable ( &meesp_buffer );
+    fini_3level_ztable ( &meesp_buffer );
     // }
 
   }  // end of if g_ts_id == 0
