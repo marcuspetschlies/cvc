ad6a4cbb2a3889ee3a8131f839276af4100aed7d
Marcus <marcus.petschlies@web.de>
2018-07-03 13:57:12 +0200

    removed if 0 endif bracket

--- piN2piN_correlators.cpp
+++ piN2piN_correlators.cpp
@@ -321,8 +321,6 @@ int main(int argc, char **argv) {
 
         if ( g_verbose > 4 && io_proc == 2 ) fprintf ( stdout, "# [piN2piN_correlators] calling contract_diagram_zm4x4_field_ti_eq_co %s %d\n", __FILE__, __LINE__ );
         exitstatus = contract_diagram_zmx4x4_field_ti_eq_co ( diagram, twopoint_function_get_correlator_phase ( &(g_twopoint_function_list[i2pt]) ), nT );
-#if 0 
-#endif  // of if 0
 
         /******************************************************
          * TEST
