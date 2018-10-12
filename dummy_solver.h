13b7f5b473c7118bc8ab835415c84af34738a2b7
Marcus <marcus.petschlies@web.de>
2018-10-12 14:15:48 +0200

    added GPU_DIRECT_SOLVER option for solver

--- dummy_solver.h
+++ dummy_solver.h
@@ -1,11 +1,18 @@
 #ifndef _DUMMY_SOLVER_H
 #define _DUMMY_SOLVER_H
 
-#ifdef DUMMY_SOLVER
+#if ( defined DUMMY_SOLVER ) && ( defined GPU_DIRECT_SOLVER )
+#error exclusive XXX_SOLVER definitions
+#endif
+
+#if ( defined DUMMY_SOLVER )
 #  define _TMLQCD_INVERT dummy_solver
 namespace cvc {
-int dummy_solver ( double * const propagator, double * const source, const int op_id, int write_prop);
+// int dummy_solver ( double * const propagator, double * const source, const int op_id, int write_prop);
+int dummy_solver ( double * const propagator, double * const source, const int op_id );
 }  /* end of namespace cvc */
+#elif ( defined GPU_DIRECT_SOLVER )
+#  define _TMLQCD_INVERT invert_quda_direct
 #else
 #  define _TMLQCD_INVERT tmLQCD_invert
 #endif
