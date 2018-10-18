c7edffa6e7b09a4b459dd1ebb6312cb00eb99369
Marcus <marcus.petschlies@web.de>
2018-10-18 12:10:27 +0200

    updated spinor_scalar_product_co and spinor_scalar_product_re

--- scalar_products.h
+++ scalar_products.h
@@ -3,8 +3,8 @@
 
 namespace cvc {
 
-void spinor_scalar_product_re(double *r, double *xi, double *phi, int V);
-void spinor_scalar_product_co(complex *w, double *xi, double *phi, int V);
+void spinor_scalar_product_re ( double * const r, double * const xi, double * const phi, unsigned int const V);
+void spinor_scalar_product_co( complex * const w, double * const xi, double * const phi, unsigned int const V);
 
 void eo_spinor_spatial_scalar_product_co( double _Complex * w, double * const xi, double * const phi, int const eo);
 
