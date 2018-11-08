f8da91876611ac88220af6184823a2a15c69d666
Bartosz Kostrzewa <bartosz_kostrzewa@fastmail.com>
2018-11-08 20:17:44 +0100

    update _TMLQCD_INVERT macro to work with current tmLQCD version without interface changes

--- dummy_solver.h
+++ dummy_solver.h
@@ -1,6 +1,8 @@
 #ifndef _DUMMY_SOLVER_H
 #define _DUMMY_SOLVER_H
 
+#include "tmLQCD.h"
+
 #if ( defined DUMMY_SOLVER ) && ( defined GPU_DIRECT_SOLVER )
 #error exclusive XXX_SOLVER definitions
 #endif
@@ -12,9 +14,11 @@ namespace cvc {
 int dummy_solver ( double * const propagator, double * const source, const int op_id );
 }  /* end of namespace cvc */
 #elif ( defined GPU_DIRECT_SOLVER )
-#  define _TMLQCD_INVERT invert_quda_direct
+#  define _TMLQCD_INVERT(PROP, SOURCE, OP_ID) \
+  invert_quda_direct((PROP), (SOURCE), (OP_ID));
 #else
-#  define _TMLQCD_INVERT tmLQCD_invert
+#  define _TMLQCD_INVERT(PROP, SOURCE, OP_ID) \
+  tmLQCD_invert((PROP), (SOURCE), (OP_ID), 0);
 #endif
 
 #endif
