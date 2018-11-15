6fd071c8f445ae73b163d4e0a0c4761eb33d4c62
Bartosz Kostrzewa <bartosz_kostrzewa@fastmail.com>
2018-11-15 17:53:35 +0100

    make sure that MPI is initialised in test programs, if requierd

--- tests/test_MT19937_64.cpp
+++ tests/test_MT19937_64.cpp
@@ -5,7 +5,14 @@
 // 2^28
 #define N_TEST 268435456ULL
 
-int main(void){
+#ifdef HAVE_MPI
+#include <mpi.h>
+#endif
+
+int main(int argc, char** argv){
+#ifdef HAVE_MPI
+  MPI_Init(&argc, &argv);
+#endif
   const unsigned long long init_key[4]={0x12345ULL, 0x23456ULL, 0x34567ULL, 0x45678ULL};
   const unsigned long long length_key = 4ULL;
   cvc::MT19937_64 rangen(init_key, length_key);
@@ -18,5 +25,9 @@ int main(void){
   // http://www.math.sci.hiroshima-u.ac.jp/~m-mat/MT/mt19937-64.out.txt
   // 7266447313870364031 4946485549665804864 [...]  
   std::cout << "First two values in test sequence: " << testvals[0] << " " << testvals[1] << std::endl;
+#ifdef HAVE_MPI
+  MPI_Finalize();
+#endif
+  return 0;
 }
 
