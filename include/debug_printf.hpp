640ed1cc1a2a04dedbe4d3c77039613c9cf28cbc
Bartosz Kostrzewa <bartosz_kostrzewa@fastmail.com>
2018-11-16 15:09:40 +0100

    port tm_debug_printf to CVC

---
+++ include/debug_printf.hpp
@@ -0,0 +1,35 @@
+/***********************************************************************
+ *  
+ * Copyright (C) 2018 Bartosz Kostrzewa
+ *
+ ***********************************************************************/
+
+#ifndef DEBUG_PRINTF_HPP
+#define DEBUG_PRINTF_HPP
+
+#include "global.h"
+#include <cstdarg>
+
+/* Function along the lines of printf which produces output on a single
+ * or all MPI tasks (unordered) when g_debug_level is at or
+ * above the provided threshold 
+ * to have output by all MPI tasks, simply pass g_proc_id for proc_id */
+
+namespace cvc {
+
+static inline void debug_printf(const int proc_id,
+                                const int verbosity_level_threshold,
+                                const char * format,
+                                ...)
+{
+  if( g_proc_id == proc_id && g_verbose >= verbosity_level_threshold ){
+    va_list arglist;
+    va_start(arglist, format);
+    vprintf(format, arglist);
+    va_end(arglist);
+  }
+}
+
+} // namespace(cvc)
+
+#endif
