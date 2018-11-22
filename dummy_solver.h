acedc5c32c9cb19074d4351ce88f4ae07e9fad4c
Marcus <marcus.petschlies@web.de>
2018-11-22 11:10:46 +0100

    added define of _TMLQCD_INVERT_TBC

--- dummy_solver.h
+++ dummy_solver.h
@@ -7,6 +7,7 @@
 
 #if ( defined DUMMY_SOLVER ) || ( ! defined HAVE_TMLQCD_LIBWRAPPER ) 
 #  define _TMLQCD_INVERT dummy_solver
+#  define _TMLQCD_INVERT_TBC dummy_solver
 namespace cvc {
 // int dummy_solver ( double * const propagator, double * const source, const int op_id, int write_prop);
 int dummy_solver ( double * const propagator, double * const source, int const op_id );
@@ -14,6 +15,7 @@ int dummy_solver ( double * const propagator, double * const source, int const o
 #elif ( defined HAVE_TMLQCD_LIBWRAPPER ) 
 #if ( defined GPU_DIRECT_SOLVER )
 #  define _TMLQCD_INVERT invert_quda_direct
+#  define _TMLQCD_INVERT_TBC(_p,_s,_o) invert_quda_direct_theta( (_p), (_s), (_o),theta_x,theta_y,theta_z,theta_t)
 #elif 
 #  define _TMLQCD_INVERT tmLQCD_invert
 #endif
