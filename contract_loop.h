5c58635d33273ed29617587865edbba23f65d6ed
Marcus <marcus.petschlies@web.de>
2018-11-23 15:13:08 +0100

    updated prototype of contract_local_loop_stochastic

--- contract_loop.h
+++ contract_loop.h
@@ -9,6 +9,6 @@
  ****************************************************/
 namespace cvc {
 
-int contract_local_loop_stochastic ( double *** const loop, double * const source, double * const prop );
+int contract_local_loop_stochastic ( double *** const loop, double * const source, double * const prop, int const momentum_number, int ( * const momentum_list)[3] );
 
 }  /* end of namespace cvc */
