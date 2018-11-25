6645aa2be396e8e78e7ec2065381f27ba6a5f0db
Marcus Petschlies <marcus.petschlies@web.de>
2018-11-25 17:58:21 +0100

    added prototype for contract_loop_write_to_h5_file;
    added missing #endif

--- contract_loop.h
+++ contract_loop.h
@@ -11,4 +11,7 @@ namespace cvc {
 
 int contract_local_loop_stochastic ( double *** const loop, double * const source, double * const prop, int const momentum_number, int ( * const momentum_list)[3] );
 
+int contract_loop_write_to_h5_file (double *** const loop, void * file, char*tag, int const momentum_number, int const nc, int const io_proc );
+
 }  /* end of namespace cvc */
+#endif
