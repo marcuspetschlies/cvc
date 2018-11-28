31c4942e5f2231ae3d738ecd25020f7b2ebe947c
Marcus <marcus.petschlies@web.de>
2018-11-28 16:19:20 +0100

    added helper prog for testing inversion+redidual check

---
+++ test_invert.cpp
@@ -0,0 +1,347 @@
+/****************************************************
+ * test_invert
+ *
+ * PURPOSE:
+ * DONE:
+ * TODO:
+ ****************************************************/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+#ifdef HAVE_MPI
+#  include <mpi.h>
+#endif
+#ifdef HAVE_OPENMP
+#  include <omp.h>
+#endif
+#include <getopt.h>
+
+#ifdef HAVE_LHPC_AFF
+#include "lhpc-aff.h"
+#endif
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#  ifdef HAVE_TMLQCD_LIBWRAPPER
+#    include "tmLQCD.h"
+#  endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#define MAIN_PROGRAM
+
+#include "cvc_complex.h"
+#include "cvc_linalg.h"
+#include "global.h"
+#include "cvc_geometry.h"
+#include "cvc_utils.h"
+#include "mpi_init.h"
+#include "set_default.h"
+#include "io.h"
+#include "propagator_io.h"
+#include "read_input_parser.h"
+#include "contractions_io.h"
+#include "Q_phi.h"
+#include "Q_clover_phi.h"
+#include "contract_cvc_tensor.h"
+#include "prepare_source.h"
+#include "prepare_propagator.h"
+#include "project.h"
+#include "table_init_z.h"
+#include "table_init_d.h"
+#include "dummy_solver.h"
+#include "clover.h"
+#include "ranlxd.h"
+
+#define _OP_ID_UP 0
+#define _OP_ID_DN 1
+#define _OP_ID_ST 2
+
+using namespace cvc;
+
+void usage() {
+  fprintf(stdout, "Usage:    [options]\n");
+  EXIT(0);
+}
+
+int main(int argc, char **argv) {
+  
+  const char outfile_prefix[] = "test";
+
+  int c;
+  int filename_set = 0;
+  int exitstatus;
+  int io_proc = -1;
+  int check_propagator_residual = 1;
+  size_t sizeof_spinor_field;
+  char filename[100];
+  // double ratime, retime;
+  double **mzz[2] = { NULL, NULL }, **mzzinv[2] = { NULL, NULL };
+  double *gauge_field_with_phase = NULL;
+  int op_id_up = -1, op_id_dn = -1;
+  char output_filename[400];
+  int * rng_state = NULL;
+
+
+#ifdef HAVE_MPI
+  MPI_Init(&argc, &argv);
+#endif
+
+  while ((c = getopt(argc, argv, "h?f:")) != -1) {
+    switch (c) {
+    case 'f':
+      strcpy(filename, optarg);
+      filename_set=1;
+      break;
+    case 'h':
+    case '?':
+    default:
+      usage();
+      break;
+    }
+  }
+
+  g_the_time = time(NULL);
+
+  /* set the default values */
+  if(filename_set==0) sprintf ( filename, "%s.input", outfile_prefix );
+  /* fprintf(stdout, "# [test_invert] Reading input from file %s\n", filename); */
+  read_input_parser(filename);
+
+#ifdef HAVE_TMLQCD_LIBWRAPPER
+
+  fprintf(stdout, "# [test_invert] calling tmLQCD wrapper init functions\n");
+
+  /***************************************************************************
+   * initialize MPI parameters for cvc
+   ***************************************************************************/
+  exitstatus = tmLQCD_invert_init(argc, argv, 1, 0);
+  if(exitstatus != 0) {
+    EXIT(1);
+  }
+  exitstatus = tmLQCD_get_mpi_params(&g_tmLQCD_mpi);
+  if(exitstatus != 0) {
+    EXIT(2);
+  }
+  exitstatus = tmLQCD_get_lat_params(&g_tmLQCD_lat);
+  if(exitstatus != 0) {
+    EXIT(3);
+  }
+#endif
+
+  /***************************************************************************
+   * initialize MPI parameters for cvc
+   ***************************************************************************/
+  mpi_init(argc, argv);
+
+  /***************************************************************************
+   * report git version
+   ***************************************************************************/
+  if ( g_cart_id == 0 ) {
+    fprintf(stdout, "# [test_invert] git version = %s\n", g_gitversion);
+  }
+
+
+  /***************************************************************************
+   * set number of openmp threads
+   ***************************************************************************/
+  set_omp_number_threads ();
+
+  /***************************************************************************
+   * initialize geometry fields
+   ***************************************************************************/
+  if ( init_geometry() != 0 ) {
+    fprintf(stderr, "[test_invert] Error from init_geometry %s %d\n", __FILE__, __LINE__);
+    EXIT(4);
+  }
+
+  geometry();
+
+  sizeof_spinor_field    = _GSI(VOLUME) * sizeof(double);
+
+  /***************************************************************************
+   * some additional xchange operations
+   ***************************************************************************/
+  mpi_init_xchange_contraction(2);
+  mpi_init_xchange_eo_spinor ();
+
+  /***************************************************************************
+   * initialize own gauge field or get from tmLQCD wrapper
+   ***************************************************************************/
+#ifndef HAVE_TMLQCD_LIBWRAPPER
+  alloc_gauge_field(&g_gauge_field, VOLUMEPLUSRAND);
+  if(!(strcmp(gaugefilename_prefix,"identity")==0)) {
+    /* read the gauge field */
+    sprintf ( filename, "%s.%.4d", gaugefilename_prefix, Nconf );
+    if(g_cart_id==0) fprintf(stdout, "# [test_invert] reading gauge field from file %s\n", filename);
+    exitstatus = read_lime_gauge_field_doubleprec(filename);
+  } else {
+    /* initialize unit matrices */
+    if(g_cart_id==0) fprintf(stdout, "\n# [test_invert] initializing unit matrices\n");
+    exitstatus = unit_gauge_field ( g_gauge_field, VOLUME );
+  }
+  if(exitstatus != 0) {
+    fprintf ( stderr, "[test_invert] Error initializing gauge field, status was %d %s %d\n", exitstatus, __FILE__, __LINE__ );
+    EXIT(8);
+  }
+#else
+  Nconf = g_tmLQCD_lat.nstore;
+  if(g_cart_id== 0) fprintf(stdout, "[test_invert] Nconf = %d\n", Nconf);
+
+  exitstatus = tmLQCD_read_gauge(Nconf);
+  if(exitstatus != 0) {
+    EXIT(5);
+  }
+
+  exitstatus = tmLQCD_get_gauge_field_pointer( &g_gauge_field );
+  if(exitstatus != 0) {
+    EXIT(6);
+  }
+  if( g_gauge_field == NULL) {
+    fprintf(stderr, "[test_invert] Error, g_gauge_field is NULL %s %d\n", __FILE__, __LINE__);
+    EXIT(7);
+  }
+#endif
+
+  /***************************************************************************
+   * multiply the temporal phase to the gauge field
+   ***************************************************************************/
+  exitstatus = gauge_field_eq_gauge_field_ti_phase ( &gauge_field_with_phase, g_gauge_field, co_phase_up );
+  if(exitstatus != 0) {
+    fprintf(stderr, "[test_invert] Error from gauge_field_eq_gauge_field_ti_phase, status was %d %s %d\n", exitstatus, __FILE__, __LINE__);
+    EXIT(38);
+  }
+
+  /***************************************************************************
+   * check plaquettes
+   ***************************************************************************/
+  exitstatus = plaquetteria ( gauge_field_with_phase );
+  if(exitstatus != 0) {
+    fprintf(stderr, "[test_invert] Error from plaquetteria, status was %d %s %d\n", exitstatus, __FILE__, __LINE__);
+    EXIT(38);
+  }
+
+  /***************************************************************************
+   * initialize clover, mzz and mzz_inv
+   ***************************************************************************/
+  exitstatus = init_clover ( &g_clover, &mzz, &mzzinv, gauge_field_with_phase, g_mu, g_csw );
+  if ( exitstatus != 0 ) {
+    fprintf(stderr, "[test_invert] Error from init_clover, status was %d %s %d\n", exitstatus, __FILE__, __LINE__ );
+    EXIT(1);
+  }
+
+  /***************************************************************************
+   * set io process
+   ***************************************************************************/
+  io_proc = get_io_proc ();
+  if( io_proc < 0 ) {
+    fprintf(stderr, "[test_invert] Error, io proc must be ge 0 %s %d\n", __FILE__, __LINE__);
+    EXIT(14);
+  }
+  fprintf(stdout, "# [test_invert] proc%.4d has io proc id %d\n", g_cart_id, io_proc );
+
+  /***********************************************************
+   * set operator ids depending on fermion type
+   ***********************************************************/
+  if ( g_fermion_type == _TM_FERMION ) {
+    op_id_up = 0;
+    op_id_dn = 1;
+  } else if ( g_fermion_type == _WILSON_FERMION ) {
+    op_id_up = 0;
+    op_id_dn = 0;
+  }
+
+  /***************************************************************************
+   * allocate memory for spinor fields 
+   * WITH HALO
+   ***************************************************************************/
+  size_t nelem = _GSI( VOLUME+RAND );
+  double ** spinor_work  = init_2level_dtable ( 2, nelem );
+  if ( spinor_work == NULL ) {
+    fprintf(stderr, "[test_invert] Error from init_2level_dtable %s %d\n", __FILE__, __LINE__ );
+    EXIT(1);
+  }
+
+  /***************************************************************************
+   * initialize rng state
+   ***************************************************************************/
+  exitstatus = init_rng_state ( g_seed, &rng_state);
+  if ( exitstatus != 0 ) {
+    fprintf(stderr, "[test_invert] Error from init_rng_state %s %d\n", __FILE__, __LINE__ );;
+    EXIT( 50 );
+  }
+
+  memset ( spinor_work[0], 0, sizeof_spinor_field );
+  if ( g_cart_id == 0 ) {
+    // spinor_work[0][0] = 1.;
+    spinor_work[0][_GSI(1)+8] = 1.;
+    // spinor_work[0][_GSI(1)] = 1.;
+    // spinor_work[0][_GSI(2)+8] = 1.;
+  }
+
+  memset ( spinor_work[1], 0, sizeof_spinor_field );
+
+  // exitstatus = _TMLQCD_INVERT ( spinor_work[1], spinor_work[0], op_id_dn );
+  exitstatus = _TMLQCD_INVERT ( spinor_work[1], spinor_work[0], op_id_up );
+  if(exitstatus < 0) {
+    fprintf(stderr, "[test_invert] Error from invert, status was %d %s %d\n", exitstatus, __FILE__, __LINE__ );
+    EXIT(44);
+  }
+
+  if ( check_propagator_residual ) {
+    // check_residual_clover ( &(spinor_work[1]), &(spinor_work[0]), gauge_field_with_phase, mzz[op_id_dn], 1 );
+    check_residual_clover ( &(spinor_work[1]), &(spinor_work[0]), gauge_field_with_phase, mzz[op_id_up], 1 );
+  }
+
+  // exitstatus = prepare_volume_source ( double * const s, unsigned int const V);
+
+
+  fini_2level_dtable ( &spinor_work );
+
+  /***************************************************************************
+   * fini rng state
+   ***************************************************************************/
+  fini_rng_state ( &rng_state);
+
+  /***************************************************************************
+   * free the allocated memory, finalize
+   ***************************************************************************/
+
+#ifndef HAVE_TMLQCD_LIBWRAPPER
+  free(g_gauge_field);
+#endif
+  free( gauge_field_with_phase );
+
+  /* free clover matrix terms */
+  fini_clover ( &mzz, &mzzinv );
+
+  free_geometry();
+
+#ifdef HAVE_TMLQCD_LIBWRAPPER
+  tmLQCD_finalise();
+#endif
+
+#ifdef HAVE_MPI
+  mpi_fini_xchange_contraction();
+  mpi_fini_xchange_eo_spinor ();
+  mpi_fini_datatypes();
+  MPI_Finalize();
+#endif
+
+  if(g_cart_id==0) {
+    g_the_time = time(NULL);
+    fprintf(stdout, "# [test_invert] %s# [test_invert] end of run\n", ctime(&g_the_time));
+    fprintf(stderr, "# [test_invert] %s# [test_invert] end of run\n", ctime(&g_the_time));
+  }
+
+  return(0);
+
+}
