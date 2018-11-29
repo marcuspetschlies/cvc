31c7d96194d102ec6f7701a0db130b6c350e07c9
Bartosz Kostrzewa <bartosz_kostrzewa@fastmail.com>
2018-11-29 16:29:30 +0100

    add some more CVC_EXIT types to enum

--- include/enums.hpp
+++ include/enums.hpp
@@ -13,7 +13,10 @@ typedef enum latDims_t {
 typedef enum ExitCode_t {
   CVC_EXIT_SUCCESS = 0,
   CVC_EXIT_CORE_INIT_FAILURE = 1,
-  CVC_EXIT_H5_KEY_LENGTH = 2
+  CVC_EXIT_H5_KEY_LENGTH = 2,
+  CVC_EXIT_GAUGE_INIT_FAILE = 3,
+  CVC_EXIT_UTIL_FUNCTION_FAILURE = 4,
+  CVC_EXIT_MALLOC_FAILURE = 5
 } ExitCode_t;
 
 }
