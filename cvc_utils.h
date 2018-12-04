5de7776d6802e63c348da71a3b2ac6e743b17db9
Marcus <marcus.petschlies@web.de>
2018-12-04 09:52:11 +0100

    added helper function show_time to evaluate walltime

--- cvc_utils.h
+++ cvc_utils.h
@@ -3,7 +3,7 @@
  *************************************************************/
 #ifndef _CVC_UTIL_H
 #define _CVC_UTIL_H
-
+#include <sys/time.h>
 #ifdef HAVE_OPENMP
 #include <omp.h>
 #endif
@@ -235,6 +235,20 @@ inline void set_omp_number_threads (void) {
 #endif
 }  /* end of set_omp_number_threads */
 
+/***************************************************************************
+ * calculate elapsed wall-time
+ ***************************************************************************/
+inline void show_time ( struct timeval * const ta, struct timeval * const tb, char * tag, char * timer, int const io ) {
+
+  long unsigned int seconds =  tb->tv_sec  - ta->tv_sec;
+  long unsigned int useconds = tb->tv_usec - ta->tv_usec;
+  if ( useconds < 0 ) {
+    useconds += 1000000;
+    seconds--;
+  }
+  if ( io ) fprintf ( stdout, "# [%s] time for %s %lu sec %lu usec\n", tag, timer, seconds, useconds );
+
+}  /* end of show_time */
 
 }  /* end of namespace cvc */
 #endif
