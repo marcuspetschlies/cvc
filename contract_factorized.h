8ac0494899dcdfdbb5bf23825a6c97d80e4c1a85
Marcus <marcus.petschlies@web.de>
2018-12-07 11:19:09 +0100

    updated prototypes

--- contract_factorized.h
+++ contract_factorized.h
@@ -17,9 +17,13 @@ int contract_v5 (double **v5, fermion_propagator_type *prop1, fermion_propagator
 
 int contract_v6 (double **v6, fermion_propagator_type *prop1, fermion_propagator_type *prop2, fermion_propagator_type *prop3, unsigned int N );
 
-int contract_vn_momentum_projection (double***vp, double**vx, int n, int (*momentum_list)[3], int momentum_number);
+/* int contract_vn_momentum_projection (double***vp, double**vx, int n, int (*momentum_list)[3], int momentum_number); */
+int contract_vn_momentum_projection (double *** const vp, double ** const vx, int const n, const int (* const momentum_list)[3], int const momentum_number);
 
-int contract_vn_write_aff (double ***vp, int n, struct AffWriter_s*affw, char*tag, int (*momentum_list)[3], int momentum_number, int io_proc );
+
+/* int contract_vn_write_aff (double ***vp, int n, struct AffWriter_s*affw, char*tag, int (*momentum_list)[3], int momentum_number, int io_proc ); */
+int contract_vn_write_aff (double *** const vp, int const n, struct AffWriter_s*affw, char*tag, const int (* const momentum_list)[3], int const momentum_number, int const io_proc );
+
 
 }  /* end of namespace cvc */
 #endif
