b0377a86c29765c9ceea9f27a4c89d94e1ac839a
Bartosz Kostrzewa <bartosz_kostrzewa@fastmail.com>
2018-12-12 15:25:38 +0100

    complete rename of contract_twopoint_xdep_snk_gamma_only

--- impl/impl_contract_twopoint_xdep_gamma5_gamma_snk_only.hpp
+++ impl/impl_contract_twopoint_xdep_gamma5_gamma_snk_only.hpp
@@ -5,26 +5,29 @@
 #include "cvc_complex.h"
 #include "global.h"
 
-template <typename T>
-void contract_twopoint_xdep_snk_gamma_only(
-    T * const contr, const int idsink, 
-    T const * chi, T const * psi, 
+template <typename TYPE>
+void contract_twopoint_xdep_gamma5_gamma_snk_only(
+    TYPE * const contr, const int idsink, 
+    TYPE const * chi, TYPE const * psi, 
     int const stride, double const factor)
 {
+#ifdef HAVE_OPENMP
 #pragma omp parallel
+#endif
   {
-    unsigned int cidx;
     double spinor1[24], spinor2[24];
     complex w;
+    size_t iix, cidx;
     FOR_IN_PARALLEL(ix, 0, VOLUME)
     {
-      size_t cidx = ix * stride;
-      _fv_eq_gamma_ti_fv(spinor1, idsink, psi+_GSI(ix));
+      iix = _GSI(ix);
+      cidx = ix * stride;
+      _fv_eq_gamma_ti_fv(spinor1, idsink, psi+iix);
       _fv_eq_gamma_ti_fv(spinor2, 5, spinor1);
-      _co_eq_fv_dag_ti_fv(&w, chi+_GSI(ix), spinor2);
+      _co_eq_fv_dag_ti_fv(&w, chi+iix, spinor2);
 
-      contr[2*cidx  ] = factor * w.re;
-      contr[2*cidx+1] = factor * w.im;
+      contr[2*cidx  ] = (TYPE)(factor * w.re);
+      contr[2*cidx+1] = (TYPE)(factor * w.im);
     }
   } // parallel section
 }
