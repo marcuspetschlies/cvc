9d720b39d53c494dbe2201e40c8932f6f196268f
Marcus <marcus.petschlies@web.de>
2019-01-15 18:22:43 +0100

    initial commit

---
+++ cvc_timer.h
@@ -0,0 +1,28 @@
+/*************************************************************
+ * cvc_timer.h                                               *
+ *************************************************************/
+#ifndef _CVC_TIMER_H
+#define _CVC_TIMER_H
+
+#include <sys/time.h>
+
+namespace cvc {
+
+/***************************************************************************
+ * calculate elapsed wall-time
+ ***************************************************************************/
+inline void show_time ( struct timeval * const ta, struct timeval * const tb, char * tag, char * timer, int const io ) {
+
+  long int seconds =  tb->tv_sec  - ta->tv_sec;
+  long int useconds = tb->tv_usec - ta->tv_usec;
+  if ( useconds < 0 ) {
+    useconds += 1000000;
+    seconds--;
+  }
+  if ( io ) fprintf ( stdout, "# [%s] time for %s %ld sec %ld usec\n", tag, timer, seconds, useconds );
+
+}  /* end of show_time */
+
+
+}
+#endif
