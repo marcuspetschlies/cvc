29701f9936ef6455010a8a6142f1716b4bedccb2
Marcus <marcus.petschlies@web.de>
2019-02-06 14:15:43 +0100

    some corrections; irrep reference index rotation check works for vector spinor

--- test_ref_rotation2.cpp
+++ test_ref_rotation2.cpp
@@ -78,13 +78,34 @@ int main(int argc, char **argv) {
   /* int (* const set_rot_mat_table ) ( rot_mat_table_type*, const char*, const char*) = set_rot_mat_table_cubic_group_single_cover; */
 #endif
 
-
   int c;
   int filename_set = 0;
   int exitstatus;
   char filename[200];
   double ratime, retime;
   FILE *ofs = NULL;
+
+  /***********************************************************
+   * set Cg basis projection coefficients
+   ***********************************************************/
+  double const Cgamma_basis_matching_coeff[16] = {
+    1.00,  /*  0 =  Cgy        */
+   -1.00,  /*  1 =  Cgzg5      */
+   -1.00,  /*  2 =  Cg0        */
+    1.00,  /*  3 =  Cgxg5      */
+    1.00,  /*  4 =  Cgyg0      */
+   -1.00,  /*  5 =  Cgyg5g0    */
+    1.00,  /*  6 =  Cgyg5      */
+   -1.00,  /*  7 =  Cgz        */
+    1.00,  /*  8 =  Cg5g0      */
+    1.00,  /*  9 =  Cgx        */
+    1.00,  /* 10 =  Cgzg5g0    */
+    1.00,  /* 11 =  C          */
+   -1.00,  /* 12 =  Cgxg5g0    */
+   -1.00,  /* 13 =  Cgxg0      */
+    1.00,  /* 14 =  Cg5        */
+    1.00   /* 15 =  Cgzg0      */
+  };
 
 #ifdef HAVE_MPI
   MPI_Init(&argc, &argv);
@@ -157,6 +178,11 @@ int main(int argc, char **argv) {
   for ( int i = 0; i < 16; i++ ) {
     gamma_matrix_set ( &(gamma[i]), i, 1. );
   }
+
+  gamma_matrix_type gdelta[3];
+  gamma_matrix_set ( gdelta,   9, Cgamma_basis_matching_coeff[9] );
+  gamma_matrix_set ( gdelta+1, 0, Cgamma_basis_matching_coeff[0] );
+  gamma_matrix_set ( gdelta+2, 7, Cgamma_basis_matching_coeff[7] );
    
   /******************************************************
    * loop on 2-point functions
@@ -224,7 +250,7 @@ int main(int argc, char **argv) {
     int refframerot             = -1;        /* reference frame rotation added below */
     int row_target              = -1;        /* no target row */
     int cartesian_list[2]       = { 0, 1 };  /* not cartesian */
-    int parity_list[2]          = { 1, 1 };  /* intrinsic parity is +1 */
+    int parity_list[2]          = { 1, -1 };  /* intrinsic parity is +1 */
     const int ** momentum_list  = NULL;      /* no momentum list given */
     int bispinor_list[2]        = { 1, 0 };  /* bispinor yes / no */
     int J2_list[2]              = { 1, 2 };  /* 2 x spin + 1 = dim ( / 2 for bispinor ) */
@@ -375,10 +401,16 @@ int main(int argc, char **argv) {
          ******************************************************/
         double _Complex ** R1 = init_2level_ztable ( spinor_dim, spinor_dim );
         double _Complex ** R2 = init_2level_ztable ( spinor_dim, spinor_dim );
-        double _Complex ** R3 = init_2level_ztable ( spinor_dim, spinor_dim );
+
+        double _Complex **** RCR = init_4level_ztable ( vector_dim, vector_dim, spinor_dim, spinor_dim );
+        if ( C == NULL ) {
+          fprintf ( stderr, "[test_ref_rotation2] Error from init_4level_ztable %s %d\n", __FILE__, __LINE__ );
+          EXIT(1);
+        }
 
         for ( int ivl = 0; ivl < vector_dim; ivl++ ) {
 
+#if 0
           /* find rotated vector index  */
           double _Complex * vl_vec  = init_1level_ztable ( vector_dim );
           double _Complex * vl_vec2 = init_1level_ztable ( vector_dim );
@@ -390,14 +422,16 @@ int main(int argc, char **argv) {
           for ( int i = 0; i < vector_dim; i++ ) {
             if ( cabs(vl_vec2[i]) > 1.e-10 ) {
               iRvl = i;
-              zRvl = vl_vec2[i];
+              zRvl = vl_vec2[i] * ( ( irotl < nrot ) ? 1. : ( projector.parity[0] * projector.parity[1] ) );
               break;
             }
           }
           fprintf ( stdout, "# [test_ref_rotation2] Rl e_%d = ( %16.7e + I %16.7e )   e_%d\n", creal(zRvl), cimag(zRvl), ivl, iRvl );
+#endif  /* of if 0 */
 
         for ( int ivr = 0; ivr < vector_dim; ivr++ ) {
 
+#if 0
           /* find rotated vector index  */
           double _Complex * vr_vec  = init_1level_ztable ( vector_dim );
           double _Complex * vr_vec2 = init_1level_ztable ( vector_dim );
@@ -409,44 +443,111 @@ int main(int argc, char **argv) {
           for ( int i = 0; i < vector_dim; i++ ) {
             if ( cabs(vr_vec2[i]) > 1.e-10 ) {
               iRvr = i;
-             zRvr = vr_vec2[i]; 
+              zRvr = vr_vec2[i] * ( ( irotr < nrot ) ? 1. : ( projector.parity[0] * projector.parity[1] ) ); 
               break;
             }
           }
           fprintf ( stdout, "# [test_ref_rotation2] Rr e_%d = ( %16.7e + I %16.7e )   e_%d\n", creal(zRvr), cimag(zRvr), ivr, iRvr );
+#endif  /* of if 0 */
 
           /******************************************************
            * apply spin rotation
            ******************************************************/
+#if 0
+          for ( int i = 0; i < vector_dim; i++ ) {
+          for ( int k = 0; k < vector_dim; k++ ) {
+
+            /* R1 = C x Sr */
+            rot_mat_ti_mat ( R1, C[i][k], Sr, spinor_dim );
+
+            /* R2 = Sl^+ x R1 */
+            rot_mat_adj_ti_mat ( R2, Sl, R1, spinor_dim );
+
+            /* RCR <- R2 * Rl_i,ivl^* Rr_k,ivr */
+            rot_mat_pl_eq_mat_ti_co ( RCR[ivl][ivr], R2, conj(Rl[i][ivl]) * Rr[k][ivr] , spinor_dim );
+
+          }}
+#endif  /* of if 0 */
+
+          gamma_matrix_type Sgl, Sgr, gl, gr;
+          gamma_matrix_init ( &gl );
+          gamma_matrix_init ( &gr );
+          gamma_matrix_init ( &Sgl );
+          gamma_matrix_init ( &Sgr );
+
+          memcpy ( Sgl.v, Sl[0], 16*sizeof(double _Complex ) );
+          memcpy ( Sgr.v, Sr[0], 16*sizeof(double _Complex ) );
+
+          /* Sgl^C C g_ivl Sgl^H */
+          gamma_eq_gamma_op_ti_gamma_matrix_ti_gamma_op ( &gl, &Sgl, 'C', &(gdelta[ivl]), &Sgl, 'H' );
+          int igl = ( gl.id == gdelta[0].id ) ? 0 : ( ( gl.id == gdelta[1].id ) ? 1 : ( ( gl.id == gdelta[2].id ) ? 2 : -1 ) );
+          if ( igl == -1 ) {
+            fprintf ( stderr, "[test_ref_rotation2] Error from gamma index matching igl = %d\n", igl );
+            EXIT(12);
+          }
+          double _Complex zgl = gl.s * Cgamma_basis_matching_coeff[gl.id];
+
+          /* Sgr^C C g_ivr Sgr^H */
+          gamma_eq_gamma_op_ti_gamma_matrix_ti_gamma_op ( &gr, &Sgr, 'C', &(gdelta[ivr]), &Sgr, 'H' );
+          int igr = ( gr.id == gdelta[0].id ) ? 0 : ( ( gr.id == gdelta[1].id ) ? 1 : ( ( gr.id == gdelta[2].id ) ? 2 : -1 ) );
+          if ( igr == -1 ) {
+            fprintf ( stderr, "[test_ref_rotation2] Error from gamma index matching igr = %d\n", igr );
+            EXIT(12);
+          }
+          double _Complex zgr = gr.s * Cgamma_basis_matching_coeff[gr.id];
+
+#if 0
+          if ( g_verbose > 2 ) fprintf ( stdout, "vrot r %2d %2d   v %d %d   il %2d %2d   ir %2d %2d   zl %16.6e %16.7e   %16.6e %16.7e   zr %16.6e %16.7e   %16.6e %16.7e\n",
+              irotl, irotr, ivl, ivr, iRvl, igl, iRvr, igr, creal(zRvl), cimag(zRvl), creal(zgl), cimag(zgl), creal(zRvr), cimag(zRvr), creal(zgr), cimag(zgr) );
+#endif  /* of if 0 */
+
+          if ( g_verbose > 2 ) fprintf ( stdout, "vrot r %2d %2d   v %d %d   il %2d    ir %2d    zl %16.6e %16.7e   zr %16.6e %16.7e\n",
+              irotl, irotr, ivl, ivr, igl, igr, creal(zgl), cimag(zgl), creal(zgr), cimag(zgr) );
+
+          /******************************************************
+           *
+           ******************************************************/
+#if 0
           /* R1 = C x Sr */
-          rot_mat_ti_mat ( R1, C[iRvl][iRvr], Sr, dim[1] );
+          rot_mat_ti_mat ( R1, C[iRvl][iRvr], Sr, spinor_dim );
 
           /* R2 = Sl^+ x R1 */
-          rot_mat_adj_ti_mat ( R2, Sl, R1, dim[1]);
+          rot_mat_adj_ti_mat ( R2, Sl, R1, spinor_dim );
 
-          contract_diagram_mat_op_ti_zm4x4_field_ti_mat_op ( &R3, Sl, 'H' , &C, Sr, 'N', 1 );
+          /* RCR <- R2 * Rl_i,ivl^* Rr_k,ivr */
+          rot_mat_pl_eq_mat_ti_co ( RCR[ivl][ivr], R2, conj( zRvl ) * zRvr , spinor_dim );
+#endif  /* of if 0 */
 
-        /******************************************************
-         * projection variants
-         ******************************************************/
+          /* R1 = C x Sr */
+          rot_mat_ti_mat ( R1, C[igl][igr], Sr, spinor_dim );
 
-        /******************************************************
-         * irrep matrix for left-applied rotation
-         *   = sink side
-         ******************************************************/
-        double _Complex ** Tirrepl = ( irotl < nrot ) ? projector.rtarget->R[irotl] : projector.rtarget->IR[irotl-nrot];
+          /* R2 = Sl^+ x R1 */
+          rot_mat_adj_ti_mat ( R2, Sl, R1, spinor_dim );
 
-        /******************************************************
-         * irrep matrix for right-applied rotation
-         *   = source side
-         ******************************************************/
-        double _Complex ** Tirrepr = ( irotr < nrot ) ? projector.rtarget->R[irotr] : projector.rtarget->IR[irotr-nrot];
+          /* RCR <- R2 * Rl_i,ivl^* Rr_k,ivr */
+          rot_mat_pl_eq_mat_ti_co ( RCR[ivl][ivr], R2, conj( zgl ) * zgr , spinor_dim );
 
-        for ( int ref_snk = 0; ref_snk < irrep_dim; ref_snk++ ) {
-        for ( int ref_src = 0; ref_src < irrep_dim; ref_src++ ) {
+          /******************************************************
+           * projection variants
+           ******************************************************/
 
-          for ( int row_snk = 0; row_snk < irrep_dim; row_snk++ ) {
-          for ( int row_src = 0; row_src < irrep_dim; row_src++ ) {
+          /******************************************************
+           * irrep matrix for left-applied rotation
+           *   = sink side
+           ******************************************************/
+          double _Complex ** Tirrepl = ( irotl < nrot ) ? projector.rtarget->R[irotl] : projector.rtarget->IR[irotl-nrot];
+
+          /******************************************************
+           * irrep matrix for right-applied rotation
+           *   = source side
+           ******************************************************/
+          double _Complex ** Tirrepr = ( irotr < nrot ) ? projector.rtarget->R[irotr] : projector.rtarget->IR[irotr-nrot];
+
+          for ( int ref_snk = 0; ref_snk < irrep_dim; ref_snk++ ) {
+          for ( int ref_src = 0; ref_src < irrep_dim; ref_src++ ) {
+
+            for ( int row_snk = 0; row_snk < irrep_dim; row_snk++ ) {
+            for ( int row_src = 0; row_src < irrep_dim; row_src++ ) {
 
 
               /******************************************************
@@ -455,14 +556,10 @@ int main(int argc, char **argv) {
                * from rotation+basis projection of gamma matrices
                ******************************************************/
               double _Complex const zcoeff = Tirrepl[row_snk][ref_snk] * conj ( Tirrepr[row_src][ref_src] );
-                
-              /* contract_diagram_zm4x4_field_eq_zm4x4_field_pl_zm4x4_field_ti_co ( 
-                  &(Cproj[ref_snk][ref_src][row_snk][row_src]), 
-                  &(Cproj[ref_snk][ref_src][row_snk][row_src]), &R2, zcoeff, 1 ); */
 
-              rot_mat_pl_eq_mat_ti_co ( Cproj[ref_snk][ref_src][row_snk][row_src][ivl][ivr].c[0][0], R2, zcoeff, spinor_dim );
+              rot_mat_pl_eq_mat_ti_co ( Cproj[ref_snk][ref_src][row_snk][row_src][ivl][ivr].c[0][0], RCR[ivl][ivr], zcoeff, spinor_dim );
 
-              fprintf ( stdout, "# [test_ref_rotation2] rotl %d %2d rotr %d %2d ref %2d %2d row %2d %2d vec %2d %2d z %25.16e %25.16e\n", 
+              if ( g_verbose > 2 ) fprintf ( stdout, "# [test_ref_rotation2] rotl %d %2d rotr %d %2d ref %2d %2d row %2d %2d vec %2d %2d z %25.16e %25.16e\n", 
                   irotl/nrot, irotl%nrot, irotr/nrot, irotr%nrot,
                   ref_snk, ref_src, row_snk, row_src, ivl, ivr, dgeps ( creal(zcoeff), 1.e-14 ), dgeps ( cimag(zcoeff), 1.e-14 ) );
 
@@ -472,21 +569,22 @@ int main(int argc, char **argv) {
         }  // end of loop on ref_src
         }  // end of loop on ref_snk
 #if 0
-#endif  /* of if 0 */
-
           fini_1level_ztable ( &vr_vec );
           fini_1level_ztable ( &vr_vec2 );
+#endif  /* of if 0 */
 
         }  // end of loop on ivr
 
+#if 0
           fini_1level_ztable ( &vl_vec );
           fini_1level_ztable ( &vl_vec2 );
+#endif  /* of if 0 */
 
         }  // end of loop on ivl
 
-        fini_2level_ztable ( &R1 );
+        fini_4level_ztable ( &RCR );
         fini_2level_ztable ( &R2 );
-        fini_2level_ztable ( &R3 );
+        fini_2level_ztable ( &R1 );
 
       }  // end of loop on source rotations
     }  // end of loop on sink rotations
@@ -512,12 +610,11 @@ int main(int argc, char **argv) {
     }}
 
 
-#if 0
     /******************************************************
      * check the rotation property
      ******************************************************/
-    twopoint_function_check_reference_rotation ( Cproj, &projector, 1.e-12 );
-
+    twopoint_function_check_reference_rotation_vector_spinor ( Cproj[0][0][0][0][0], &projector, 5.e-12 );
+#if 0
 #endif  /* of if 0 */
 
     /******************************************************
