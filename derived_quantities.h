1dd0718954c9bae8c6ddd371cdbf7acb22728be0
Marcus <marcus.petschlies@web.de>
2019-07-02 17:55:03 +0200

    added cumulants

--- derived_quantities.h
+++ derived_quantities.h
@@ -1,6 +1,12 @@
 #ifndef _DERIVED_QUANTITIES_H
 #define _DERIVED_QUANTITIES_H
 
+#define _POW2(_a) (      (_a) * (_a) )
+#define _POW3(_a) ( _POW2(_a) * (_a) )
+#define _POW4(_a) ( _POW3(_a) * (_a) )
+#define _POW5(_a) ( _POW4(_a) * (_a) )
+#define _POW6(_a) ( _POW5(_a) * (_a) )
+
 namespace cvc {
 
 int ratio_1_1 ( void * param , void * v_in, double * v_out);
@@ -24,6 +30,15 @@ int acosh_ratio_deriv ( void * param , void * v_in, double * v_out);
 int a_mi_b_ti_c ( void * param , void * v_in, double * v_out);
 
 int da_mi_b_ti_c ( void * param , void * v_in, double * v_out);
+
+int  cumulant_1  ( void * param , void * v_in, double * v_out);
+int dcumulant_1  ( void * param , void * v_in, double * v_out);
+int  cumulant_2  ( void * param , void * v_in, double * v_out);
+int dcumulant_2  ( void * param , void * v_in, double * v_out);
+int  cumulant_3  ( void * param , void * v_in, double * v_out);
+int dcumulant_3  ( void * param , void * v_in, double * v_out);
+int  cumulant_4  ( void * param , void * v_in, double * v_out);
+int dcumulant_4  ( void * param , void * v_in, double * v_out);
 
 }
 
