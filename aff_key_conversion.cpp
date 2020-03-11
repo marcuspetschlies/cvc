c0373f4a2f0a9f4003292f6c06b69224a2013448
Marcus <marcus.petschlies@web.de>
2020-03-11 08:52:26 +0100

    added use_oet option and adapted key format

--- aff_key_conversion.cpp
+++ aff_key_conversion.cpp
@@ -36,6 +36,8 @@ const int C_gamma_to_gamma[16][2] = {
  *************************************************************************************/
 void aff_key_conversion (char*key, char * const tag, int const i_sample, int const pi2[3], int const pf1[3], int const pf2[3], int const source_coords[4], int const gamma_id, int const C_gamma_id,  int const i_spin ) {
 
+  int const use_oet = 1;
+
   int C_gid = ( C_gamma_id >= 0 ) ? C_gamma_id : ( gamma_id >= 0 ? C_gamma_to_gamma[gamma_id][0] : -1 );
   if ( C_gid == -1 ) {
     fprintf ( stderr, "[aff_key_conversion] Error, could not make sense out of input gamma_id = %d and C_gamma_id = %d\n", gamma_id, C_gamma_id );
@@ -45,65 +47,73 @@ void aff_key_conversion (char*key, char * const tag, int const i_sample, int con
 
   if ( strcmp( tag, "w_1_xi") == 0 ) {
     /* /w_1_xi/sample03/pf2x00pf2y00pf2z-01/t09x06y07z00 */
+    const char * stoch_field = use_oet ? "g5.phi" : "xi";
 
-    sprintf( key, "/v3/t%.2dx%.2dy%.2dz%.2d/xi-g%.2d-u/sample%.2d/px%.2dpy%.2dpz%.2d",
-        source_coords[0], source_coords[1], source_coords[2], source_coords[3],
+    sprintf( key, "/v3/t%.2dx%.2dy%.2dz%.2d/%s-g%.2d-u/sample%.2d/px%.2dpy%.2dpz%.2d",
+        source_coords[0], source_coords[1], source_coords[2], source_coords[3], stoch_field,
         C_gid, i_sample,
         pf2[0], pf2[1], pf2[2]);
 
   } else if ( strcmp( tag, "w_1_phi") == 0 ) {
     /* /w_1_phi/sample02/pi2x00pi2y00pi2z01/pf1x00pf1y00pf1z01/t09x06y07z00/g06 */
+    const char * stoch_field = use_oet ? "g5.xi" : "phi";
 
-    sprintf(key, "/v2/t%.2dx%.2dy%.2dz%.2d/pi2x%.2dpi2y%.2dpi2z%.2d/phi-g%.2d-u-ud/sample%.2d/px%.2dpy%.2dpz%.2d",
+    sprintf(key, "/v2/t%.2dx%.2dy%.2dz%.2d/pi2x%.2dpi2y%.2dpi2z%.2d/%s-g%.2d-u-ud/sample%.2d/px%.2dpy%.2dpz%.2d",
         source_coords[0], source_coords[1], source_coords[2], source_coords[3],
-        pi2[0], pi2[1], pi2[2], C_gid, i_sample,
+        pi2[0], pi2[1], pi2[2], stoch_field, C_gid, i_sample,
         pf1[0], pf1[1], pf1[2]);
 
   } else if ( strcmp( tag, "w_3_phi") == 0 ) {
     /* /w_3_phi/sample03/pi2x00pi2y00pi2z-01/pf1x00pf1y00pf1z-01/t01x02y03z04/g04 */
+    const char * stoch_field = use_oet ? "g5.xi" : "phi";
 
-    sprintf(key, "/v2/t%.2dx%.2dy%.2dz%.2d/pi2x%.2dpi2y%.2dpi2z%.2d/phi-g%.2d-ud-u/sample%.2d/px%.2dpy%.2dpz%.2d",
+    sprintf(key, "/v2/t%.2dx%.2dy%.2dz%.2d/pi2x%.2dpi2y%.2dpi2z%.2d/%s-g%.2d-ud-u/sample%.2d/px%.2dpy%.2dpz%.2d",
         source_coords[0], source_coords[1], source_coords[2], source_coords[3],
-        pi2[0], pi2[1], pi2[2], C_gid, i_sample,
+        pi2[0], pi2[1], pi2[2], stoch_field, C_gid, i_sample,
         pf1[0], pf1[1], pf1[2]);
 
   } else if ( strcmp( tag, "b_1_xi") == 0 ) {
     /* /b_1_xi/sample03/pi2x00pi2y00pi2z-01/pf2x00pf2y00pf2z-01/t09x06y07z00 */
+    const char * stoch_field = use_oet ? "g5.phi" : "xi";
 
-    sprintf(key, "/v3/t%.2dx%.2dy%.2dz%.2d/pi2x%.2dpi2y%.2dpi2z%.2d/xi-g%.2d-ud/sample%.2d/px%.2dpy%.2dpz%.2d",
+    sprintf(key, "/v3/t%.2dx%.2dy%.2dz%.2d/pi2x%.2dpi2y%.2dpi2z%.2d/%s-g%.2d-ud/sample%.2d/px%.2dpy%.2dpz%.2d",
         source_coords[0], source_coords[1], source_coords[2], source_coords[3],
-        pi2[0], pi2[1], pi2[2], C_gid, i_sample,
+        pi2[0], pi2[1], pi2[2], stoch_field, C_gid, i_sample,
         pf2[0], pf2[1], pf2[2] );
 
   } else if ( strcmp( tag, "b_1_phi") == 0 ) {
     /* /b_1_phi/sample03/pf1x00pf1y00pf1z01/t09x06y07z00/g06 */
+    const char * stoch_field = use_oet ? "g5.xi" : "phi";
 
-    sprintf(key, "/v2/t%.2dx%.2dy%.2dz%.2d/phi-g%.2d-u-u/sample%.2d/px%.2dpy%.2dpz%.2d",
-        source_coords[0], source_coords[1], source_coords[2], source_coords[3],
+    sprintf(key, "/v2/t%.2dx%.2dy%.2dz%.2d/%s-g%.2d-u-u/sample%.2d/px%.2dpy%.2dpz%.2d",
+        source_coords[0], source_coords[1], source_coords[2], source_coords[3], stoch_field,
         C_gid, i_sample,
         pf1[0], pf1[1], pf1[2]);
 
   } else if ( strcmp( tag, "z_3_phi") == 0 ) {
     /* /z_3_phi/sample00/pi2x00pi2y00pi2z-01/pf1x00pf1y00pf1z-01/t09x06y07z00/g06 */
+    const char * stoch_field = "phi";
 
-    sprintf(key, "/v2-oet/t%.2dx%.2dy%.2dz%.2d/pi2x%.2dpi2y%.2dpi2z%.2d/phi-g%.2d-d-u/sample%.2d/d%d/px%.2dpy%.2dpz%.2d",
+    sprintf(key, "/v2-oet/t%.2dx%.2dy%.2dz%.2d/pi2x%.2dpi2y%.2dpi2z%.2d/%s-g%.2d-d-u/sample%.2d/d%d/px%.2dpy%.2dpz%.2d",
         source_coords[0], source_coords[1], source_coords[2], source_coords[3],
-        pi2[0], pi2[1], pi2[2], C_gid, i_sample, i_spin,
+        pi2[0], pi2[1], pi2[2], stoch_field, C_gid, i_sample, i_spin,
         pf1[0], pf1[1], pf1[2]);
 
   } else if ( strcmp( tag, "z_1_phi") == 0 ) {
     /* /z_1_phi/sample00/pi2x00pi2y00pi2z-01/pf1x00pf1y00pf1z-01/t01x02y03z04/g06 */
+    const char * stoch_field = "phi";
 
-    sprintf(key, "/v4-oet/t%.2dx%.2dy%.2dz%.2d/pi2x%.2dpi2y%.2dpi2z%.2d/phi-g%.2d-d-u/sample%.2d/d%d/px%.2dpy%.2dpz%.2d", 
+    sprintf(key, "/v4-oet/t%.2dx%.2dy%.2dz%.2d/pi2x%.2dpi2y%.2dpi2z%.2d/%s-g%.2d-d-u/sample%.2d/d%d/px%.2dpy%.2dpz%.2d", 
         source_coords[0], source_coords[1], source_coords[2], source_coords[3],
-        pi2[0], pi2[1], pi2[2], C_gid, i_sample, i_spin, pf1[0], pf1[1], pf1[2]);
+        pi2[0], pi2[1], pi2[2], stoch_field, C_gid, i_sample, i_spin, pf1[0], pf1[1], pf1[2]);
 
   } else if ( strcmp( tag, "z_1_xi") == 0 ) {
     /* /z_1_xi/sample00/pf2x00pf2y00pf2z-01/t09x06y07z00 */
+    const char * stoch_field = "g5.phi";
 
-    sprintf(key, "/v3-oet/t%.2dx%.2dy%.2dz%.2d/pi2x%.2dpi2y%.2dpi2z%.2d/phi-g%.2d-u/sample%.2d/d%d/px%.2dpy%.2dpz%.2d",
+    sprintf(key, "/v3-oet/t%.2dx%.2dy%.2dz%.2d/pi2x%.2dpi2y%.2dpi2z%.2d/%s-g%.2d-u/sample%.2d/d%d/px%.2dpy%.2dpz%.2d",
         source_coords[0], source_coords[1], source_coords[2], source_coords[3],
-        pi2[0], pi2[1], pi2[2], C_gid, i_sample, i_spin,
+        pi2[0], pi2[1], pi2[2], stoch_field, C_gid, i_sample, i_spin,
         pf2[0], pf2[1], pf2[2] );
   }
 
