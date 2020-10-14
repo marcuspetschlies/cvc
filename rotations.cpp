69bb74dfdf10bd628169c07f68c0e3c8b42932dc
Marcus Petschlies <marcus.petschlies@web.de>
2020-10-14 20:22:36 +0200

    modified parity matrix, to be conform with plegma gamma basis

--- rotations.cpp
+++ rotations.cpp
@@ -2101,7 +2101,8 @@ void rot_inversion_matrix_spherical_basis ( double _Complex**R, int J2, int bisp
 
   if ( ( J2 == 1 && bispinor ) || ( J2 == 3 ) ) {
     gamma_matrix_type g;
-    gamma_matrix_set ( &g, 0, 1 );
+    // gamma_matrix_set ( &g, 0, 1 );
+    gamma_matrix_set ( &g, 5, 1 );
 
     memcpy ( R[0], g.v, 16*sizeof(double _Complex) );
     return;
