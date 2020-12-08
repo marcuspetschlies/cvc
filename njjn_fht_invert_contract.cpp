eee3f8865e0d6a0c9c8a11d81511522aebe90a89
Marcus Petschlies <marcus.petschlies@web.de>
2020-12-08 12:41:36 +0100

    corrected seq prop name character for b,d

--- njjn_fht_invert_contract.cpp
+++ njjn_fht_invert_contract.cpp
@@ -951,7 +951,7 @@ int main(int argc, char **argv) {
             EXIT(132);
           }
 
-          char const sequential_propagator_name = ( seq_source_type == 0 ) ? 'b' : 'd';
+          char const sequential_propagator_name = ( seq_source_type == 0 ) ? 'd' : 'b';
 
 
           /***************************************************************************
