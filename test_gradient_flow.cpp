dd4b49c4841f37eed1d96f043366b04f2ea6acff
Marcus Petschlies <marcus.petschlies@web.de>
2021-07-29 11:38:40 +0200

    added helper prog for testing GF routines

---
+++ test_gradient_flow.cpp
@@ -0,0 +1,360 @@
+/***************************************************************************
+ *
+ * test_gradient_flow
+ *
+ ***************************************************************************/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+#include <complex.h>
+#ifdef HAVE_MPI
+#  include <mpi.h>
+#endif
+#ifdef HAVE_OPENMP
+#  include <omp.h>
+#endif
+#include <getopt.h>
+
+#ifdef HAVE_LHPC_AFF
+#include "lhpc-aff.h"
+#endif
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#  ifdef HAVE_TMLQCD_LIBWRAPPER
+#    include "tmLQCD.h"
+#  endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#define MAIN_PROGRAM
+
+#include "cvc_complex.h"
+#include "cvc_linalg.h"
+#include "global.h"
+#include "cvc_geometry.h"
+#include "cvc_utils.h"
+#include "cvc_timer.h"
+#include "mpi_init.h"
+#include "set_default.h"
+#include "io.h"
+#include "propagator_io.h"
+#include "read_input_parser.h"
+#include "smearing_techniques.h"
+#include "contractions_io.h"
+#include "Q_clover_phi.h"
+#include "prepare_source.h"
+#include "prepare_propagator.h"
+#include "project.h"
+#include "table_init_z.h"
+#include "table_init_d.h"
+#include "dummy_solver.h"
+#include "contractions_io.h"
+#include "contract_factorized.h"
+#include "contract_diagrams.h"
+#include "gamma.h"
+#include "clover.h"
+#include "gradient_flow.h"
+
+using namespace cvc;
+
+/***************************************************************************
+ * helper message
+ ***************************************************************************/
+void usage() {
+  fprintf(stdout, "Code for FHT-type nucleon-nucleon 2-pt function inversion and contractions\n");
+  fprintf(stdout, "Usage:    [options]\n");
+  fprintf(stdout, "Options:  -f input <filename> : input filename for [default cpff.input]\n");
+  fprintf(stdout, "          -c                  : check propagator residual [default false]\n");
+  fprintf(stdout, "          -h                  : this message\n");
+  EXIT(0);
+}
+
+/***************************************************************************
+ *
+ * MAIN PROGRAM
+ *
+ ***************************************************************************/
+int main(int argc, char **argv) {
+  
+  const char outfile_prefix[] = "test_gf";
+
+  int c;
+  int filename_set = 0;
+  int exitstatus;
+  int io_proc = -1;
+  char filename[400];
+  struct timeval ta, tb, start_time, end_time;
+
+#ifdef HAVE_LHPC_AFF
+  struct AffWriter_s *affw = NULL;
+  char aff_tag[400];
+#endif
+
+#ifdef HAVE_MPI
+  MPI_Init(&argc, &argv);
+#endif
+
+  while ((c = getopt(argc, argv, "h?f:")) != -1) {
+    switch (c) {
+    case 'f':
+      strcpy(filename, optarg);
+      filename_set=1;
+      break;
+    case 'h':
+    case '?':
+    default:
+      usage();
+      break;
+    }
+  }
+
+  gettimeofday ( &start_time, (struct timezone *)NULL );
+
+
+  /***************************************************************************
+   * read input and set the default values
+   ***************************************************************************/
+  if(filename_set==0) strcpy(filename, "twopt.input");
+  read_input_parser(filename);
+
+#ifdef HAVE_TMLQCD_LIBWRAPPER
+
+  fprintf(stdout, "# [test_gradient_flow] calling tmLQCD wrapper init functions\n");
+
+  /***************************************************************************
+   * initialize tmLQCD solvers
+   ***************************************************************************/
+  exitstatus = tmLQCD_invert_init(argc, argv, 1, 0);
+  if(exitstatus != 0) {
+    EXIT(1);
+  }
+  exitstatus = tmLQCD_get_mpi_params(&g_tmLQCD_mpi);
+  if(exitstatus != 0) {
+    EXIT(2);
+  }
+  exitstatus = tmLQCD_get_lat_params(&g_tmLQCD_lat);
+  if(exitstatus != 0) {
+    EXIT(3);
+  }
+#endif
+
+  /***************************************************************************
+   * initialize MPI parameters for cvc
+   ***************************************************************************/
+  mpi_init(argc, argv);
+  mpi_init_xchange_contraction(2);
+
+  /***************************************************************************
+   * report git version
+   * make sure the version running here has been commited before program call
+   ***************************************************************************/
+  if ( g_cart_id == 0 ) {
+    fprintf(stdout, "# [test_gradient_flow] git version = %s\n", g_gitversion);
+  }
+
+  /***************************************************************************
+   * set number of openmp threads
+   *
+   *   each process and thread reports
+   ***************************************************************************/
+#ifdef HAVE_OPENMP
+  if(g_cart_id == 0) fprintf(stdout, "# [test_gradient_flow] setting omp number of threads to %d\n", g_num_threads);
+  omp_set_num_threads(g_num_threads);
+#pragma omp parallel
+{
+  fprintf(stdout, "# [test_gradient_flow] proc%.4d thread%.4d using %d threads\n", g_cart_id, omp_get_thread_num(), omp_get_num_threads());
+}
+#else
+  if(g_cart_id == 0) fprintf(stdout, "[test_gradient_flow] Warning, resetting global thread number to 1\n");
+  g_num_threads = 1;
+#endif
+
+  if ( init_geometry() != 0 ) {
+    fprintf(stderr, "[test_gradient_flow] Error from init_geometry %s %d\n", __FILE__, __LINE__);
+    EXIT(4);
+  }
+
+  
+  /***************************************************************************
+   * initialize lattice geometry
+   *
+   * allocate and fill geometry arrays
+   ***************************************************************************/
+  geometry();
+
+
+  /***************************************************************************
+   * set up some mpi exchangers for
+   * (1) even-odd decomposed spinor field
+   * (2) even-odd decomposed propagator field
+   ***************************************************************************/
+  mpi_init_xchange_eo_spinor();
+  mpi_init_xchange_eo_propagator();
+
+  /***************************************************************************
+   * set up the gauge field
+   *
+   *   either read it from file or get it from tmLQCD interface
+   *
+   *   lime format is used
+   ***************************************************************************/
+#ifndef HAVE_TMLQCD_LIBWRAPPER
+  alloc_gauge_field(&g_gauge_field, VOLUMEPLUSRAND);
+  if(!(strcmp(gaugefilename_prefix,"identity")==0)) {
+    /* read the gauge field */
+    sprintf ( filename, "%s.%.4d", gaugefilename_prefix, Nconf );
+    if(g_cart_id==0) fprintf(stdout, "# [test_gradient_flow] reading gauge field from file %s\n", filename);
+    exitstatus = read_lime_gauge_field_doubleprec(filename);
+  } else {
+    /* initialize unit matrices */
+    if(g_cart_id==0) fprintf(stdout, "\n# [test_gradient_flow] initializing unit matrices\n");
+    exitstatus = unit_gauge_field ( g_gauge_field, VOLUME );
+  }
+#else
+  Nconf = g_tmLQCD_lat.nstore;
+  if(g_cart_id== 0) fprintf(stdout, "[test_gradient_flow] Nconf = %d\n", Nconf);
+
+  exitstatus = tmLQCD_read_gauge(Nconf);
+  if(exitstatus != 0) {
+    EXIT(5);
+  }
+
+  exitstatus = tmLQCD_get_gauge_field_pointer( &g_gauge_field );
+  if(exitstatus != 0) {
+    EXIT(6);
+  }
+  if( g_gauge_field == NULL) {
+    fprintf(stderr, "[test_gradient_flow] Error, g_gauge_field is NULL %s %d\n", __FILE__, __LINE__);
+    EXIT(7);
+  }
+#endif
+
+  /***********************************************************
+   * multiply the phase to the gauge field
+   ***********************************************************/
+  /* double *gauge_field_with_phase = NULL;
+  exitstatus = gauge_field_eq_gauge_field_ti_phase ( &gauge_field_with_phase, g_gauge_field, co_phase_up );
+  if(exitstatus != 0) {
+    fprintf(stderr, "[test_gradient_flow] Error from gauge_field_eq_gauge_field_ti_phase, status was %d %s %d\n", exitstatus, __FILE__, __LINE__);
+    EXIT(38);
+  } */
+
+  /***************************************************************************
+   * initialize the clover term, 
+   * lmzz and lmzzinv
+   *
+   *   mzz = space-time diagonal part of the Dirac matrix
+   *   l   = light quark mass
+   ***************************************************************************/
+  /* double **lmzz[2] = { NULL, NULL }, **lmzzinv[2] = { NULL, NULL };
+  exitstatus = init_clover ( &lmzz, &lmzzinv, gauge_field_with_phase );
+  if ( exitstatus != 0 ) {
+    fprintf(stderr, "[test_gradient_flow] Error from init_clover, status was %d %s %d\n", exitstatus, __FILE__, __LINE__ );
+    EXIT(1);
+  } */
+
+  /***************************************************************************
+   * set io process
+   ***************************************************************************/
+  io_proc = get_io_proc ();
+  if( io_proc < 0 ) {
+    fprintf(stderr, "[test_gradient_flow] Error, io proc must be ge 0 %s %d\n", __FILE__, __LINE__);
+    EXIT(14);
+  }
+  fprintf(stdout, "# [test_gradient_flow] proc%.4d has io proc id %d\n", g_cart_id, io_proc );
+
+  /***************************************************************************
+   * prepare the Fourier phase field
+   ***************************************************************************/
+  unsigned int const VOL3 = LX * LY * LZ;
+  size_t const sizeof_gauge_field = 72 * ( VOLUME ) * sizeof( double );
+  size_t const sizeof_spinor_field = _GSI( VOLUME ) * sizeof( double );
+
+  /***************************************************************************
+   * init rng state
+   ***************************************************************************/
+  exitstatus = init_rng_stat_file ( g_seed, NULL );
+  if ( exitstatus != 0 ) {
+    fprintf(stderr, "[test_gradient_flow] Error from init_rng_stat_file %s %d\n", __FILE__, __LINE__ );;
+    EXIT( 50 );
+  }
+
+  /***************************************************************************
+   *
+   * 
+   *
+   ***************************************************************************/
+
+  double ** spinor_field = init_2level_dtable ( 2, _GSI( VOLUME ) );
+  
+  double * gauge_field_smeared = init_1level_dtable ( 72 * VOLUME );
+
+  unsigned int const gf_niter = 100;
+  double const gf_dt = 0.01;
+
+  memcpy ( gauge_field_smeared, g_gauge_field, sizeof_gauge_field );
+
+  for ( unsigned int i = 0; i < gf_niter; i++ ) {
+
+    gettimeofday ( &ta, (struct timezone *)NULL );
+
+    flow_fwd_gauge_spinor_field ( gauge_field_smeared, NULL, 1, gf_dt, 1, 0 );
+
+    gettimeofday ( &tb, (struct timezone *)NULL );
+    show_time ( &ta, &tb, "test_gradient_flow", "flow_fwd_gauge_spinor_field", g_cart_id == 0 );
+
+
+    exitstatus = plaquetteria  ( gauge_field_smeared );
+  }
+
+  fini_2level_dtable ( &spinor_field );
+  fini_1level_dtable ( &gauge_field_smeared );
+
+  flow_fwd_gauge_spinor_field ( NULL, NULL, 0, 0., 0, 0 );
+
+
+  /***************************************************************************/
+  /***************************************************************************/
+
+  /***************************************************************************
+   * free the allocated memory, finalize
+  ***************************************************************************/
+
+#ifndef HAVE_TMLQCD_LIBWRAPPER
+  if ( g_gauge_field != NULL ) free(g_gauge_field);
+#endif
+  /* if ( gauge_field_with_phase != NULL ) free ( gauge_field_with_phase ); */
+
+  /* free clover matrix terms */
+  fini_clover ( );
+
+  /* free lattice geometry arrays */
+  free_geometry();
+
+#ifdef HAVE_TMLQCD_LIBWRAPPER
+  tmLQCD_finalise();
+#endif
+
+
+#ifdef HAVE_MPI
+  mpi_fini_xchange_contraction();
+  mpi_fini_xchange_eo_spinor();
+  mpi_fini_datatypes();
+  MPI_Finalize();
+#endif
+
+  gettimeofday ( &end_time, (struct timezone *)NULL );
+  show_time ( &start_time, &end_time, "test_gradient_flow", "runtime", g_cart_id == 0 );
+
+  return(0);
+
+}
