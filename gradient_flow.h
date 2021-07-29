8f8c702c73f5965ee132699c69438d62f9ef6e76
Marcus Petschlies <marcus.petschlies@web.de>
2021-07-29 11:39:36 +0200

    updated prototypes

--- gradient_flow.h
+++ gradient_flow.h
@@ -1,4 +1,5 @@
 #ifndef _GRADIENT_FLOW_H
+#define _GRADIENT_FLOW_H
 
 namespace cvc {
 
@@ -6,9 +7,9 @@ void ZX ( double * const z, double * const g, double const a, double const b );
 
 void apply_ZX ( double * const g, double * const z, double const dt );
 
-int apply_laplace ( double * const s, double * const r_in, double * const g );
+void apply_laplace ( double * const s, double * const r_in, double * const g );
 
-void flow_fwd_gauge_spinor_field ( double * const g, double * const phi, unsigned int const niter, double const dt );
+void flow_fwd_gauge_spinor_field ( double * const g, double * const phi, unsigned int const niter, double const dt, int const flow_gauge, int const flow_spinor );
 
 }
 
