a2abfe84185a944b7e2d9e761fedf97026032928
Marcus Petschlies <marcus.petschlies@web.de>
2021-11-01 16:37:27 +0100

    upated init_clover and fini_clover to multiple calls ( from cpff branch)

--- clover.h
+++ clover.h
@@ -3,9 +3,10 @@
 
 namespace cvc {
 
-int init_clover ( double **(*mzz)[2], double **(*mzzinv)[2], double*gauge_field );
+int init_clover ( double *** clover_term, double **(*mzz)[2], double **(*mzzinv)[2], double*gauge_field, double const mass, double const csw );
 
-void fini_clover(void);
+
+void fini_clover ( double **(*mzz)[2], double **(*mzzinv)[2] );
 
 }
 #endif
