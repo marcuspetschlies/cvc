a352cdf612fedd1c0f2f9bdd2064df674bdb3d57
Marcus <marcus.petschlies@web.de>
2023-09-29 18:00:48 +0300

    update of lime2ascii

--- lime2ascii.cpp
+++ lime2ascii.cpp
@@ -75,15 +75,16 @@ int main(int argc, char **argv) {
   char limefile_name[100] = "NA";
   char limefile_suffix[100] = "inverted";
   char limefile_type[100] = "DiracFermion";
-  int limefile_pos = 0;
   int tsize = 0, lsize = 0;
+  int components = 0;
+  int limefile_position = 0;
   // double ratime, retime;
 
 #ifdef HAVE_MPI
   MPI_Init(&argc, &argv);
 #endif
 
-  while ((c = getopt(argc, argv, "h?f:l:t:p:T:L:")) != -1) {
+  while ((c = getopt(argc, argv, "h?f:l:t:p:T:L:c:")) != -1) {
     switch (c) {
     case 'f':
       strcpy(filename, optarg);
@@ -96,13 +97,16 @@ int main(int argc, char **argv) {
       strcpy ( limefile_type, optarg );
       break;
     case 'p':
-      limefile_pos = atoi ( optarg );
+      limefile_position = atoi ( optarg );
       break;
     case 'T':
       tsize = atoi ( optarg );
       break;
     case 'L':
       lsize = atoi ( optarg );
+      break;
+    case 'c':
+      components = atoi ( optarg );
       break;
     case 'h':
     case '?':
@@ -171,7 +175,7 @@ int main(int argc, char **argv) {
      ***************************************************************************/
 
     double * spinor_field = init_1level_dtable ( _GSI(VOLUME) );
-    exitstatus = read_lime_spinor ( spinor_field, limefile_name, limefile_pos);
+    exitstatus = read_lime_spinor ( spinor_field, limefile_name, limefile_position);
 
     if ( exitstatus != 0 ) {
       fprintf(stderr, "[lime2ascii] Error from read_lime_spinor, status was %d %s %d\n", exitstatus, __FILE__, __LINE__);
@@ -237,7 +241,7 @@ int main(int argc, char **argv) {
     double ** propagator_field = init_2level_dtable ( 12, _GSI(VOLUME) );
     for ( int i = 0; i < 12; i++ ) {
       sprintf ( filename, "%s.%d.%s", limefile_name, i, limefile_suffix );
-      exitstatus = read_lime_spinor ( propagator_field[i], filename, limefile_pos);
+      exitstatus = read_lime_spinor ( propagator_field[i], filename, limefile_position);
 
       if ( exitstatus != 0 ) {
         fprintf(stderr, "[lime2ascii] Error from read_lime_spinor, status was %d %s %d\n", exitstatus, __FILE__, __LINE__);
@@ -270,7 +274,102 @@ int main(int argc, char **argv) {
     fclose ( ofs );
 
     fini_2level_dtable ( &propagator_field );
+
+  } else if ( strncmp ( limefile_type, "Contraction", 11 ) == 0 ) {
+
+    /***************************************************************************
+     * write as generic contraction field
+     ***************************************************************************/
+    int const nc = components;
+    if ( nc == 0 ) {
+      fprintf ( stderr, "[lime2ascii] Error, number of components is zero %s %d\n", __FILE__, __LINE__ );
+      EXIT(2);
+    } else {
+      fprintf ( stdout, "# [lime2ascii] using type = %s with nc = %d double complex %s %d\n", limefile_type, nc, __FILE__, __LINE__ );
+    }
+
+    double ** field = init_2level_dtable ( VOLUME, 2 * nc );
+
+    if ( limefile_position == -1 ) limefile_position = 0;
+
+    exitstatus = read_lime_contraction( field[0], limefile_name, nc, limefile_position );
+    if ( exitstatus != 0 ) {
+      fprintf(stderr, "[lime2ascii] Error from read_lime_spinor, status was %d %s %d\n", exitstatus, __FILE__, __LINE__);
+      EXIT(2);
+    }
+
+    sprintf ( filename,"%s.pos%d.ascii", limefile_name, limefile_position );
+    FILE * ofs = fopen ( filename, "w" );
+
+    for ( int x0 = 0; x0 < T; x0++ ) {
+    for ( int x1 = 0; x1 < LX; x1++ ) {
+    for ( int x2 = 0; x2 < LY; x2++ ) {
+    for ( int x3 = 0; x3 < LZ; x3++ ) {
+      unsigned int const ix = g_ipt[x0][x1][x2][x3];
+      fprintf ( ofs, "# [lime2ascii] x %3d %3d %3d %3d\n", x0, x1, x2, x3 );
+
+
+      for ( int i = 0; i < nc; i++ ) {
+        fprintf ( ofs, "s %3d  %25.16e + %25.16e I\n", i, field[ix][2*i], field[ix][2*i+1] );
+      }
+
+    }}}}
+
+    fclose ( ofs );
+
+    fini_2level_dtable ( &field );
+
+  } else if ( strncmp ( limefile_type, "SZIN", 4 ) == 0 ) {
+
+    /***************************************************************************
+     * write as generic contraction field
+     ***************************************************************************/
+    int const nc = 144;
+    if ( nc == 0 ) {
+      fprintf ( stderr, "[lime2ascii] Error, number of components is zero %s %d\n", __FILE__, __LINE__ );
+      EXIT(2);
+    } else {
+      fprintf ( stdout, "# [lime2ascii] using type = %s with nc = %d double complex %s %d\n", limefile_type, nc, __FILE__, __LINE__ );
+    }
+
+    double * field = init_1level_dtable ( VOLUME * 2 * nc );
+
+    if ( limefile_position == -1 ) limefile_position = 0;
+
+    exitstatus = read_lime_propagator ( field, limefile_name, limefile_position);
+    if ( exitstatus != 0 ) {
+      fprintf(stderr, "[lime2ascii] Error from read_lime_propagator for file \"%s\", status was %d %s %d\n", limefile_name, exitstatus, __FILE__, __LINE__);
+      EXIT(2);
+    }
+
+    sprintf ( filename,"%s.ascii", limefile_name);
+    FILE * ofs = fopen ( filename, "w" );
+
+    for ( int x0 = 0; x0 < T; x0++ ) {
+    for ( int x1 = 0; x1 < LX; x1++ ) {
+    for ( int x2 = 0; x2 < LY; x2++ ) {
+    for ( int x3 = 0; x3 < LZ; x3++ ) {
+      unsigned int const ix = g_ipt[x0][x1][x2][x3];
+      fprintf ( ofs, "# [lime2ascii] x %3d %3d %3d %3d\n", x0, x1, x2, x3 );
+
+
+      for ( int isc = 0; isc < 12; isc++ ) {
+        for ( int ir = 0; ir < 12; ir++ ) {
+
+          int const is = 3 * ( 3 * ( 4 * (ir/3) + (isc/3) ) + (ir%3) ) + (isc%3);
+
+          fprintf ( ofs, "  %3d  %3d   %25.16e   %25.16e\n", isc, ir, field[2*(nc*ix + is)], field[2*(nc*ix + is) + 1] );
+        }
+        fprintf ( ofs, "\n" );
+      }
+
+    }}}}
+
+    fclose ( ofs );
+
+    fini_1level_dtable ( &field );
   }
+
 
   free_geometry();
 
