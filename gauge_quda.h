e786bb6635c1fe9243ab5ae8befdcdf6c30cff53
Marcus <marcus.petschlies@web.de>
2024-04-29 17:24:29 +0200

    removed fermion field reordering

--- gauge_quda.h
+++ gauge_quda.h
@@ -15,8 +15,6 @@ void gauge_field_qdp_to_cvc ( double * g_cvc, double ** g_qdp );
 
 void init_gauge_param (QudaGaugeParam * const gauge_param );
 
-void spinor_field_cvc_to_quda ( double * const r, double * const s );
-
 }
 #endif  /* _GFLOW_QUDA */
 #endif
