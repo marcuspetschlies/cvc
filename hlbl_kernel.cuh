a90bc97666cc70b2bd4afe20a21b646a0bdfd434
Marcus Petschlies <mpetschl@login02.leonardo.local>
2024-05-17 11:48:20 +0200

    added cu kernel file; renamed header files to cuh cuda header files

---
+++ hlbl_kernel.cuh
@@ -0,0 +1,18 @@
+#ifndef _HLBL_KERNEL_CUH
+#define _HLBL_KERNEL_CUH
+
+#ifdef __restrict__
+#define _RESTR __restrict__
+#else
+#define _RESTR
+#endif
+
+#ifndef CUDA_BLOCK_SIZE
+#  define CUDA_BLOCK_SIZE 256
+#endif
+
+// void ker_X_prepare_ev ( cuDoubleComplex* _RESTR out, const double* _RESTR in, const double *_RESTR kerv, const int N );
+
+__global__ void ker_X_prepare_ev ( cuDoubleComplex* _RESTR out, const double* _RESTR in, const double *_RESTR kerv, const int N );
+
+#endif
